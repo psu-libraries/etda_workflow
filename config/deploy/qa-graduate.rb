29e8b58eefea9034ad9e5d9a56b174a9898e541d
jxb13@psu.edu <jxb13@psu.edu>
2017-10-25 07:46:38 -0400

    Added submission_status class to hold status methods from the submission class. Refs ETDA-982, ETDA-985 Enhanced tests for submission statuses.

--- config/deploy/qa-graduate.rb
+++ config/deploy/qa-graduate.rb
@@ -1,5 +1,5 @@
 set :stage, fetch(:vstage, 'qa')
-set :deploy_to, "/opt/heracles/deploy/#{application}_graduate"
+set :deploy_to, "/opt/heracles/deploy/#{application}-graduate"
 role :web,  "etdaworkflow1dev.vmhost.psu.edu:1855"
 role :app,  "etdaworkflow1dev.vmhost.psu.edu:1855"
 role :solr, "etdaworkflow1dev.vmhost.psu.edu:1855" # This is where resolrize will run
