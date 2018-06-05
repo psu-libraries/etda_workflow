8129d94f09438434bf8fb2a15e47a2b0fbed0eff
Jim Campbell <jkc103@psu.edu>
2018-06-05 14:08:45 -0400

    APPINT-2291

---
+++ config/deploy/qa.honors.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# config/deploy/qa.honors.rb
+set :stage, 'qa'
+set :partner, 'honors'
+set :deploy_to, "/opt/deploy/etda_workflow_honors"
+set :tmp_dir, "/opt/deploy/etda_workflow_honors/tmp"
+role :web,  "etdaworkflow1qa.vmhost.psu.edu:1855"
+role :app,  "etdaworkflow1qa.vmhost.psu.edu:1855"
+role :db,   "etdaworkflow1qa.vmhost.psu.edu:1855", primary: true # This is where Rails migrations will run
+server 'etdaworkflow1qa.vmhost.psu.edu:1855', user: 'deploy', roles: %w[web]
