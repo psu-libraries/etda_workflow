5fa1679160c62b41e39fbfd126b2e577d2f74918
jxb13@psu.edu <jxb13@psu.edu>
2018-06-13 12:38:09 -0400

    ETDA-1110 Fixed bug that was occuring when admin edited a final submission.  Fixed  admin/submissions_controller update method so that final submission attributes are updated when a final submission file is edited.

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -74,7 +74,7 @@ module StubbedAuthenticationHelper
 end
 
 RSpec.configure do |config|
-  config.before do
+  config.after do
     Warden::Strategies.add :webaccess_authenticatable, Devise::Strategies::WebaccessAuthenticatable
     StubbedAuthenticationStrategy.author = nil unless @current_author.nil?
     StubbedAuthenticationStrategy.admin = nil unless @current_admin.nil?
