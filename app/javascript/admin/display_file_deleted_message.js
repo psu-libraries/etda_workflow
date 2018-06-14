4eb0bc410ac4904a5cb84821f2c76227d3991e40
jxb13@psu.edu <jxb13@psu.edu>
2018-06-14 07:21:33 -0400

    ETDA-611 changed wording of delete file message.

--- app/javascript/admin/display_file_deleted_message.js
+++ app/javascript/admin/display_file_deleted_message.js
@@ -6,7 +6,7 @@ setup_delete_file_link = () =>
     $('a#file_delete_link').on('keypress, click', function(e){
         fname = jQuery.trim($(this).next('a.file-link').html()).split('\n')[0];
         put_msg_here = $(this).closest('div.links.cocoon-links');
-        put_msg_here.prepend("<p class='alert-danger'><strong>The file:  " + fname + " will be deleted when one of the buttons below is clicked.</strong></p>")
+        put_msg_here.prepend("<p class='alert-danger'>The file, " + fname + ", will be deleted when the form is submitted.  Click 'Cancel' if you do not want to delete the file.</p>")
     });
 
 $(document).ready(setup_delete_file_link);
