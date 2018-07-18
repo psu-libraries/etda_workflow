fd786518b1f2980ed7c5dccad207644001fcdfd2
jxb13@psu.edu <jxb13@psu.edu>
2018-07-18 14:47:48 -0400

    Enhanced solr import commands so that the command waits until processing is complete and handles errors.

--- app/models/solr_data_import_service.rb
+++ app/models/solr_data_import_service.rb
@@ -1,17 +1,36 @@
 class SolrDataImportService
   def delta_import
     #  https://etda-explore-partner/solr/partner_core/dataimport?wt=json&command=delta-import&clean=false
-    response = solr.get dataimport, params: delta_import_params
+    # response = solr.get dataimport, params: delta_import_params
+    response = execute_cmd(delta_import_params)
     response
   end
 
   def full_import
     #  https://etda-explore-partner/solr/partner_core/dataimport?wt=json&command=full-import&clean=true
-    response = solr.get dataimport, params: full_import_params
+    # response = solr.get dataimport, params: full_import_params
+    response = execute_cmd(full_import_params)
     response
   end
 
   private
+
+  def execute_cmd(params)
+    result = solr.get dataimport, params: params
+    return result if result[:error]
+
+    # wait until the process has finished
+    processing_is_not_complete = true
+    while processing_is_not_complete
+      sleep(10.0)
+      processing_results = solr.get dataimport, params: params
+      processing_is_not_complete = processing_results["status"] == "busy"
+    end
+    processing_results
+  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+    Rails.logger.error e.inspect
+    return { error: true, "statusMessages" => { "" => "An error occured!  Check the log messages for more information" } }
+  end
 
   def solr
     @solr ||= RSolr.connect url: solr_url, core: current_core
