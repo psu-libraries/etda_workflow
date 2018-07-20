47f52efa1dc4b83efd3cf166a397d8c1966fb904
jxb13@psu.edu <jxb13@psu.edu>
2018-07-20 16:00:04 -0400

    ETDA-1030 - added SolrDataImportService and SolrLog to workflow Conflicts: 	app/models/solr_data_import_service.rb 	app/services/update_submission_service.rb 	spec/models/solr_data_import_service_spec.rb

--- app/services/update_submission_service.rb
+++ app/services/update_submission_service.rb
@@ -2,9 +2,11 @@ class UpdateSubmissionService
   def self.call(submission)
     # submission.update_attributes!(submission_params)
 
-    return unless submission.access_level != submission.previous_access_level
+    return { error: false } unless submission.access_level != submission.previous_access_level
     email = AccessLevelUpdatedEmail.new(Admin::SubmissionView.new(submission, nil))
     email.deliver
+    # this occurs each time a final submisison is published or unpublished.
+    # All editing to published submissions requires unpublish then publish so this includes metadata changes that must be reindexed
     SolrDataImportService.new.delta_import if submission.current_access_level.to_i > submission.previous_access_level.to_i
   end
 end
