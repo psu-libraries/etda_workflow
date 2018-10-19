fa1d9faa450b067929494df8ea530e9ffa672279
jxb13@psu.edu <jxb13@psu.edu>
2018-10-19 16:30:07 -0400

    ETDA-1222 Fix building of urls for solr commands.  This was changed for new url (submit-etd) and was not working properly for rake tasks.

--- app/models/etd_urls.rb
+++ app/models/etd_urls.rb
@@ -1,20 +1,20 @@
 class EtdUrls
-  def base
-    return I18n.t("#{current_partner.id}.partner.url_slug") + "-explore.localhost:3000" if Rails.env.test?
-
-    ApplicationUrl.current
-  end
-
   def explore
-   explore_url
+    return "http://" + I18n.t("#{current_partner.id}.partner.url_slug") + "-explore.localhost:3000" if Rails.env.test?
+
+    explore_url
   end
 
   private
 
   def explore_url
-     return '' if base.nil?
-
-     explore_str = "https://" + I18n.t("#{current_partner.id}.partner.url_slug") + "-explore#{ApplicationUrl.stage}.libraries.psu.edu/"
+     explore_str = "https://" + I18n.t("#{current_partner.id}.partner.url_slug") + "-explore#{stage_server}.libraries.psu.edu"
      explore_str
+  end
+
+  def stage_server
+    return '' if Rails.application.secrets.stage.nil?
+
+    "-#{Rails.application.secrets.stage}"
   end
 end
