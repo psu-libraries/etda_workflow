ad9585ecd6dd9578e0ed798e08b1d53e2e66a64a
Alexander Kiessling <ajkiessl@gmail.com>
2019-02-08 11:32:08 -0500

    ETDA-1302 Changed rails.env to env[stage]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -94,7 +94,7 @@ class ApplicationController < ActionController::Base
 
   def explore_url
     hosts = EtdaUtilities::Hosts.new
-    hosts.explore_host(current_partner.id, Rails.env)
+    hosts.explore_host(current_partner.id, ENV['STAGE'])
   end
 
   protected
