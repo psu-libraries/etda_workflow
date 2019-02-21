09062648741b4d6733f1a50d11b2334bfa5024b1
Alexander Kiessling <ajkiessl@gmail.com>
2019-02-21 12:24:23 -0500

    ETDA-1309 Added verify_files_email view.  Changede verify_files_disabled to enabled spec.  Added workflow mailer spec to test verify files email sending.

---
+++ spec/lib/tasks/verify_files_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'shoulda-matchers'
+require 'active_record/fixtures'
+
+RSpec.describe "Rake::Task['final_files:verify']", type: :task do
+  before do
+    Rails.application.load_tasks
+    root_dir = Rails.root.join('tmp')
+    FileUtils.rm_rf(Dir["#{root_dir}/explore/*"])
+    FileUtils.rm_rf(Dir["#{root_dir}/workflow/*"])
+    submission1 = FactoryBot.create(:submission, :released_for_publication)
+    submission2 = FactoryBot.create(:submission, :waiting_for_publication_release)
+    FileUtils.copy(Rails.root.join('spec', 'fixtures', 'final_submission_file_01.pdf'), Rails.root.join('tmp', 'explore', 'explore_file_01.pdf'))
+    FileUtils.copy(Rails.root.join('spec', 'fixtures', 'final_submission_file_01.pdf'), Rails.root.join('tmp', 'workflow', 'workflow_file_01.pdf'))
+    FinalSubmissionFile.create(asset: File.open(Rails.root.join('tmp', 'explore', 'explore_file_01.pdf')), submission_id: submission1.id)
+    FinalSubmissionFile.create(asset: File.open(Rails.root.join('tmp', 'workflow','workflow_file_01.pdf')), submission_id: submission2.id)
+
+  end
+
+  it 'verifies location of files' do
+    Rake::Task['final_files:verify'].reenable
+
+    # allow_any_instance_of(EtdaFilePaths).to receive(:detailed_file_path).and_return('99/999')
+    num_files = FinalSubmissionFile.all.count
+    expect(Rake::Task['final_files:verify'].invoke).to receive(:locate_file).with(000).exactly(1).times.and_return('hello')
+    puts task_output
+  end
+end
