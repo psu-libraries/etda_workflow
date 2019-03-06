481f87cd3c4ad96b1de6702e8c313637cc22e443
Alexander Kiessling <ajkiessl@gmail.com>
2019-03-06 16:43:31 -0500

    EDTA-1184 Added log to sidekiq config, added fake thing to rails helper, and changed spec so it works.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ require 'rspec/rails'
 
 # sidekiq
 require 'sidekiq/testing'
+Sidekiq::Testing.fake!  # by default it is fake
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
