b376ab0daa6aab9f7385ce6a555f7938ad8dd359
Alexander Kiessling <ajkiessl@gmail.com>
2019-03-07 10:34:04 -0500

    Removed ActionJob code and configs.  Added max_retires to sidekiq.yml

--- config/initializers/active_job.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-# Be sure to restart your server when you modify this file.
-
-# Configure ActiveJob with SideKiq
-Rails.application.config.active_job.queue_adapter = :sidekiq
