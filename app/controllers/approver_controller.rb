cb73979b80a22766d1854dd9f84321610f1d982a
Alexander Kiessling <ajkiessl@gmail.com>
2019-03-20 15:39:21 -0400

    ETDA-1187 LDAP check for approvers.

--- app/controllers/approver_controller.rb
+++ app/controllers/approver_controller.rb
@@ -6,8 +6,8 @@ class ApproverController < ApplicationController
   Devise.add_module(:webaccess_authenticatable, strategy: true, controller: :sessions, model: 'devise/models/webaccess_authenticatable')
 
   before_action :clear_approver
-  before_action :find_or_initialize_approver
   before_action :authenticate_or_redirect
+  before_action :find_or_initialize_approver
 
   protected
 
@@ -23,15 +23,25 @@ class ApproverController < ApplicationController
     # only logout if the REMOTE_USER is not set in the HTTP headers and a user is set within warden
     #  logout clears the entire session including flash messages
     request.env['warden'].logout if current_remote_user.nil? || !valid_approver_session?
-    session[:user_role] = 'approver'
   end
 
   def authenticate_or_redirect
-    if current_remote_user.present?
+    if valid_approver?
       authenticate_approver! unless valid_approver_session?
     else
       redirect_to '/401' unless Rails.env.test?
     end
+  end
+
+  def valid_approver?
+    this_approver = current_remote_user
+    user_is_approver = if current_approver.nil?
+                          LdapUniversityDirectory.new.exists?(this_approver)
+                       else
+                          true
+                       end
+    session[:user_role] = 'approver' if user_is_approver
+    user_is_approver
   end
 
   def valid_approver_session?
