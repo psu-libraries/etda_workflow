d9ab5a0678bb2d768f3984f634ff6e3de7eeda7c
Alexander Kiessling <ajkiessl@gmail.com>
2019-03-26 12:10:56 -0400

    ETDA-1184-fixes Added spec that tests mail is delivered after worker call.  Spec to test nomethoderror. Fixed bug in worker.

--- spec/workers/committee_reminder_worker_spec.rb
+++ spec/workers/committee_reminder_worker_spec.rb
@@ -7,8 +7,22 @@ RSpec.describe CommitteeReminderWorker do
   let(:submission) { FactoryBot.create :submission }
 
   context "when approval process starts" do
-    it 'sends email to sidekiq via worker' do
+    it 'queues to sidekiq via worker' do
       expect { described_class.perform_in(5.days, [submission.id, "test@psu.edu"]) }.to change { Sidekiq::Worker.jobs.size }.by(1)
+    end
+
+    it 'performs task' do
+      Sidekiq::Testing.inline! do
+        expect{ described_class.perform_async(submission.id, "test@psu.edu") }.to change { WorkflowMailer.deliveries.size }.by(1)
+      end
+    end
+  end
+
+  context "when submission no longer exists" do
+    it 'NoMethodError is returned' do
+      Sidekiq::Testing.inline! do
+        expect { described_class.perform_async(submission.id + 1, "test@psu.edu") }.to raise_error(NoMethodError)
+      end
     end
   end
 end
