ff3f467013cbb606992ee60f50bf9262b1e80df0
Alexander Kiessling <ajkiessl@gmail.com>
2019-04-02 13:21:12 -0400

    ETDA-1322 'password' key, symbol to string

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { password: "#{Rails.application.config_for(:redis)[:password]}"  }
+  config.redis = { password: "#{Rails.application.config_for(:redis)['password']}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { password: "#{Rails.application.config_for(:redis)[:password]}"  }
+  config.redis = { password: "#{Rails.application.config_for(:redis)['password']}" }
 end
