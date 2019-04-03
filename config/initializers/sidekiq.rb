86ddd83f86c9c006d86d20c01dda525edec76cc5
Alexander Kiessling <ajkiessl@gmail.com>
2019-04-03 09:14:31 -0400

    ETDA-1322 Only use password when in stage/prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { password: "#{Rails.application.config_for(:redis)['password']}" }
+  config.redis = { password: "#{Rails.application.config_for(:redis)['password']}" } unless Rails.env = 'development'
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { password: "#{Rails.application.config_for(:redis)['password']}" }
+  config.redis = { password: "#{Rails.application.config_for(:redis)['password']}" } unless Rails.env = 'development'
 end
