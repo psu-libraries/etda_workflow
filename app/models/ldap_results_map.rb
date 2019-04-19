53bacc2f9922475094ba43a145e141af06997a55
Alex Kiessling <ajkiessl@gmail.com>
2019-04-19 14:45:37 -0400

    ETDA-1335 Added uid to autocomplete ldap map.

--- app/models/ldap_results_map.rb
+++ app/models/ldap_results_map.rb
@@ -4,7 +4,8 @@ class LdapResultsMap
   AUTOCOMPLETE_LDAP_MAP = { map: { displayname: %i[label value],
                                    mail: [:id],
                                    psadminarea: [:dept_admin],
-                                   psdepartment: [:dept] }, defaults: { dept: 'Department not available', id: 'Email not available' } }.freeze
+                                   psdepartment: [:dept] },
+                                   uid: [:access_id], defaults: { dept: 'Department not available', id: 'Email not available', uid: 'Access ID not available' } }.freeze
 
   AUTHOR_LDAP_MAP = { uid: [:access_id],
                       givenname: %i[first_name middle_name],
