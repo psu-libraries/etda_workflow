bf5658f7971a37bec175868fece2000ef3c5b8f2
Alex Kiessling <ajkiessl@gmail.com>
2019-05-21 13:30:03 -0400

    ETDA-1337 Format review update service spec now tests recording changes to status and voting.

--- spec/services/format_review_update_service_spec.rb
+++ spec/services/format_review_update_service_spec.rb
@@ -4,12 +4,17 @@ require 'rails_helper'
 require 'shoulda-matchers'
 
 RSpec.describe FormatReviewUpdateService, type: :model do
+  let(:committee_member) { FactoryBot.create :committee_member }
+
   context 'it processes approved format review submissions' do
     it 'approves a format review' do
-      submission = FactoryBot.create :submission, :waiting_for_format_review_response
+      submission = FactoryBot.create :submission, :waiting_for_format_review_response, committee_members: [committee_member]
       params = ActionController::Parameters.new
       params[:submission] = submission.attributes
+      params[:submission][:committee_members_attributes] = { "0" => submission.committee_members.first.attributes }
       params[:approved] = true
+      params[:submission][:committee_members_attributes]["0"]['is_voting'] = false
+      params[:submission][:committee_members_attributes]["0"]['status'] = 'approved'
       title = submission.title
       format_review_update_service = described_class.new(params, submission, 'testuser123')
       result = format_review_update_service.respond_format_review
@@ -17,15 +22,20 @@ RSpec.describe FormatReviewUpdateService, type: :model do
       expect(result[:redirect_path]).to eql("/admin/#{submission.degree_type.slug}/format_review_submitted")
       expect(submission.status).to eq('collecting final submission files')
       expect(submission.title).to eq(title)
+      expect(submission.committee_members.first.is_voting).to eq(false)
+      expect(submission.committee_members.first.status).to eq('approved')
+      expect(submission.committee_members.first.notes).to match(/\ntestuser123 changed 'status' to 'approved' at: .*\ntestuser123 changed 'is_voting' to 'false' at:/)
     end
   end
 
   context 'it processes rejected format review submissions' do
     it 'rejects a format review' do
-      submission = FactoryBot.create :submission, :waiting_for_format_review_response
+      submission = FactoryBot.create :submission, :waiting_for_format_review_response, committee_members: [committee_member]
       params = ActionController::Parameters.new
       params[:submission] = submission.attributes
+      params[:submission][:committee_members_attributes] = { "0" => submission.committee_members.first.attributes }
       params[:rejected] = true
+      params[:submission][:committee_members_attributes]["0"]['is_voting'] = false
       semester = submission.semester
       format_review_update_service = described_class.new(params, submission, 'testuser123')
       result = format_review_update_service.respond_format_review
@@ -33,18 +43,22 @@ RSpec.describe FormatReviewUpdateService, type: :model do
       expect(result[:redirect_path]).to eql("/admin/#{submission.degree_type.slug}/format_review_submitted")
       expect(submission.status).to eq('collecting format review files rejected')
       expect(submission.semester).to eq(semester)
+      expect(submission.committee_members.first.is_voting).to eq(false)
+      expect(submission.committee_members.first.notes).to match(/\ntestuser123 changed 'is_voting' to 'false' at:/)
     end
   end
 
   context 'it updates the record' do
     it 'updates a format review' do
-      submission = FactoryBot.create :submission, :waiting_for_format_review_response
+      submission = FactoryBot.create :submission, :waiting_for_format_review_response, committee_members: [committee_member]
       original_title = submission.title
       params = ActionController::Parameters.new
       params[:submission] = submission.attributes
+      params[:submission][:committee_members_attributes] = { "0" => submission.committee_members.first.attributes }
       params[:update_format_review] = true
       params[:submission][:title] = 'a different title'
       params[:submission][:format_review_notes] = 'a note to you'
+      params[:submission][:committee_members_attributes]["0"]['status'] = 'rejected'
       format_review_update_service = described_class.new(params, submission, 'testuser123')
       result = format_review_update_service.update_record
       expect(result[:msg]).to eql("The submission was successfully updated.")
@@ -52,23 +66,27 @@ RSpec.describe FormatReviewUpdateService, type: :model do
       expect(submission.title).to eq('a different title')
       expect(submission.format_review_notes).to eq('a note to you')
       expect(original_title == submission.title).to be_falsey
+      expect(submission.committee_members.first.status).to eq('rejected')
     end
   end
 
   context 'it updates a format review submission and status' do
     it 'approves a format review' do
-      submission = FactoryBot.create :submission, :waiting_for_format_review_response
+      submission = FactoryBot.create :submission, :waiting_for_format_review_response, committee_members: [committee_member]
       params = ActionController::Parameters.new
       params[:submission] = submission.attributes
+      params[:submission][:committee_members_attributes] = { "0" => submission.committee_members.first.attributes }
       params[:approved] = true
       params[:submission][:title] = 'another different title'
       params[:submission][:format_review_notes] = 'another note to you'
+      params[:submission][:committee_members_attributes]["0"]['is_voting'] = false
       format_review_update_service = described_class.new(params, submission, 'testuser123')
       result = format_review_update_service.respond_format_review
       expect(result[:msg]).to eql("The submission's format review information was successfully approved and returned to the author to collect final submission information.")
       expect(result[:redirect_path]).to eql("/admin/#{submission.degree_type.slug}/format_review_submitted")
       expect(submission.status).to eq('collecting final submission files')
       expect(submission.title == 'another different title').to be_truthy
+      expect(submission.committee_members.first.is_voting).to eq(false)
     end
   end
 end
