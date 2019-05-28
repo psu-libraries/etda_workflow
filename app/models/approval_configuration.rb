fc6a5aab334e0a06dc56111c54c6e1d5a5ba2233
Alex Kiessling <ajkiessl@gmail.com>
2019-05-28 16:44:50 -0400

    ETDA-1366 Added percentage columns to seed method. Fixed bug in method.

--- app/models/approval_configuration.rb
+++ app/models/approval_configuration.rb
@@ -5,19 +5,19 @@ class ApprovalConfiguration < ApplicationRecord
 
   validates :degree_type_id, presence: true
 
-  GRADUATE_CONFIGURATION = { 'dissertation' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0 },
-                             'master_thesis' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0 } }.freeze
+  GRADUATE_CONFIGURATION = { 'dissertation' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0, use_percentage: 0, percentage_for_approval: 100 },
+                             'master_thesis' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0, use_percentage: 0, percentage_for_approval: 100 } }.freeze
 
-  HONORS_CONFIGURATION = { 'thesis' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0 } }.freeze
+  HONORS_CONFIGURATION = { 'thesis' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0, use_percentage: 0, percentage_for_approval: 100 } }.freeze
 
-  MILSCH_CONFIGURATION = { 'thesis' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0 } }.freeze
+  MILSCH_CONFIGURATION = { 'thesis' => { approval_deadline_on: Date.today, rejections_permitted: 0, email_admins: 0, email_authors: 0, use_percentage: 0, percentage_for_approval: 100 } }.freeze
 
   CONFIGURATIONS = { 'graduate' => ApprovalConfiguration::GRADUATE_CONFIGURATION, 'honors' => ApprovalConfiguration::HONORS_CONFIGURATION, 'milsch' => ApprovalConfiguration::MILSCH_CONFIGURATION }.freeze
 
   def self.seed
     ApprovalConfiguration::CONFIGURATIONS[current_partner.id].each do |degree_type, configuration|
       dt = DegreeType.find_by(slug: degree_type)
-      next if dt.approval_configuration.blank?
+      next unless dt.approval_configuration.blank?
 
       ApprovalConfiguration.create!(degree_type_id: dt[:id],
                                     approval_deadline_on: configuration[:approval_deadline_on],
