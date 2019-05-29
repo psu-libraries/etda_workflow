528345cd5b6251eaa8ffb970695e8bd2998f9686
Natalie Nash <natalie@westarete.com>
2019-05-29 11:00:13 -0400

    ETDA-1331, 1340, 1357 - Add final status and disable review fields

--- spec/integration/approver/approver_spec.rb
+++ spec/integration/approver/approver_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Approver approval page', type: :integration, js: true do
   let(:submission) { FactoryBot.create :submission, :waiting_for_committee_review, created_at: Time.zone.now }
   let(:final_submission_file) { FactoryBot.create :final_submission_file, submission: submission }
   let(:approval_configuration) { FactoryBot.create :approval_configuration }
+  let(:committee_role) { FactoryBot.create :committee_role, id: 1 }
 
   before do
     submission.final_submission_files << final_submission_file
@@ -64,12 +65,53 @@ RSpec.describe 'Approver approval page', type: :integration, js: true do
     end
   end
 
+  context 'approver is advisor and part of graduate school' do
+    let(:committee_member) { FactoryBot.create :committee_member, committee_role: committee_role, submission: submission, access_id: 'testuser'}
+
+    before do
+      visit "approver/committee_member/#{committee_member.id}"
+    end
+
+    it 'asks about federal funding used' do
+      expect(page).to have_content('Were Federal Funds utilized for this submission?')
+    end
+  end
+
+  context 'approver is not advisor' do
+    it 'asks about federal funding used' do
+      committee_member = FactoryBot.create :committee_member, submission: submission, access_id: 'testuser'
+
+      visit "approver/committee_member/#{committee_member.id}"
+      expect(page).not_to have_content('Were Federal Funds utilized for this submission?')
+    end
+  end
+
   context 'approver is not in Ldap' do
     it 'redirects to 401 error page' do
       committee_member = FactoryBot.create :committee_member, submission: submission, access_id: 'testuser'
 
       visit "approver/committee_member/#{committee_member.id}"
       expect(page).to have_current_path('/401')
+    end
+  end
+
+  context 'approval is complete' do
+    let(:submission1) { FactoryBot.create :submission, status: "waiting for final submission response"}
+    let(:committee_member) { FactoryBot.create :committee_member, submission: submission1, access_id: 'testuser', status: "approved", notes: "this is the note", approved_at: DateTime.new(2019,10,10,5,5,55) }
+
+    before do
+      visit "approver/committee_member/#{committee_member.id}"
+    end
+
+    it 'displays the committee members response' do
+      expect(page).to have_content('approved')
+      expect(page).to have_content('Review Completed on')
+    end
+    it 'displays the ultimate outcome of the submission' do
+      expect(page).to have_content('Committee approved the submission')
+    end
+    it 'displays the help text message' do
+      expect(page).to have_content('If your response needs to be altered')
     end
   end
 end
