84d7076aad0bc68d95b120cd26184e97a14e615b
Alex Kiessling <ajkiessl@gmail.com>
2019-06-17 14:25:28 -0400

    ETDA-1396 Added base urls for workflow and specs for this.

--- spec/models/etd_urls_spec.rb
+++ spec/models/etd_urls_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe EtdUrls, type: :model do
     end
   end
 
+  context 'explore urls' do
+    it 'returns graduate url' do
+      expect(described_class.new.workflow).to eql('https://submit-etda-.libraries.psu.edu.localhost:3000/') if current_partner.graduate?
+    end
+    it 'returns honors url' do
+      expect(described_class.new.workflow).to eql('https://submit-honors-.libraries.psu.edu.localhost:3000/') if current_partner.honors?
+    end
+    it 'returns milsch url' do
+      expect(described_class.new.workflow).to eql('https://submit-millennium-scholars-.libraries.psu.edu.localhost:3000/') if current_partner.milsch?
+    end
+  end
+
+
   context '#popup' do
     it 'graduate and honors are blank' do
       expect(described_class.new.popup).to be_blank unless current_partner.milsch?
