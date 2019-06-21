95fec2220425ebe5d413439bfc28283b12c170a4
Alex Kiessling <ajkiessl@gmail.com>
2019-06-21 13:53:16 -0400

    ETDA-1390 Session for non specific session pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,12 +38,12 @@ class ApplicationController < ActionController::Base
 
   def main
     render '/main/index.html', layout: 'home'
-    session[:user_name] = 'Guest' if current_remote_user && session[:user_name].blank?
+    session[:user_name] = current_remote_user if current_remote_user && session[:user_name].blank?
   end
 
   def about
     render '/about/index.html', layout: 'home'
-    session[:user_name] = 'Guest' if current_remote_user && session[:user_name].blank?
+    session[:user_name] = current_remote_user if current_remote_user && session[:user_name].blank?
   end
 
   def login
