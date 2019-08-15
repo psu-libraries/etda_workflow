e4c50d190826f7f257dd7f3fa8aa3d401b3e7e7a
Alex Kiessling <ajkiessl@gmail.com>
2019-08-15 15:00:21 -0400

    Removed ul-etd-release address from access level emails.

--- app/services/access_level_updated_email.rb
+++ app/services/access_level_updated_email.rb
@@ -1,10 +1,6 @@
 class AccessLevelUpdatedEmail
 attr_reader :old_submission
 
-def self.cataloging_email_address
-  'ul-etd-release@lists.psu.edu'
-end
-
 def self.otm_email_address
   'otminfo@psu.edu'
 end
@@ -21,7 +17,7 @@ def submission
 end
 
 def cc_addresses
-  return [self.class.otm_email_address, self.class.cataloging_email_address] if previous_level == 'restricted' && current_level.open_access?
+  return [self.class.otm_email_address] if previous_level == 'restricted' && current_level.open_access?
 
   graduate_cc_addresses
 end
@@ -37,7 +33,7 @@ def graduate_cc_addresses
   return result unless partner.graduate?
 
   if previous_level == 'restricted_to_institution' && current_level.open_access?
-    result = [self.class.otm_email_address, self.class.cataloging_email_address]
+    result = [self.class.otm_email_address]
   elsif previous_level == 'restricted' && current_level == 'restricted_to_institution'
     result = [self.class.otm_email_address]
   end
