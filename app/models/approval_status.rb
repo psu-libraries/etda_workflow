5333bad24b66572e15300eddb30eb7e702c4b029
Alex Kiessling <ajkiessl@gmail.com>
2019-09-09 15:21:49 -0400

    ETDA-1530 Non grad bug fix.

--- app/models/approval_status.rb
+++ app/models/approval_status.rb
@@ -18,7 +18,7 @@ class ApprovalStatus
   end
 
   def head_of_program_status
-    return 'approved' if current_submission.committee_members.find_by(committee_role_id: CommitteeRole.find_by(name: 'Program Head/Chair', degree_type: current_submission.degree.degree_type).id).blank?
+    return 'approved' if !current_partner.graduate? || current_submission.committee_members.find_by(committee_role_id: CommitteeRole.find_by(name: 'Program Head/Chair', degree_type: current_submission.degree.degree_type).id).blank?
 
     current_submission.committee_members.find_by(committee_role_id: CommitteeRole.find_by(name: 'Program Head/Chair', degree_type: current_submission.degree.degree_type).id).status
   end
