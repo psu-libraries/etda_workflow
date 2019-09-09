83284637a980a505469f062db3fe900d226ea1c3
Alex Kiessling <ajkiessl@gmail.com>
2019-09-09 12:45:46 -0400

    ETDA-1529 Pop up when approving final submissions.  And spec.

---
+++ spec/integration/admin/submissions/approve_final_submission_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe "when an admin approves a final submission", js: true do
+  require 'integration/integration_spec_helper'
+
+  let(:author) { FactoryBot.create :author }
+  let(:submission) { FactoryBot.create :submission, :waiting_for_final_submission_response, author: author }
+
+  before do
+    webaccess_authorize_admin
+    visit admin_edit_submission_path(submission)
+  end
+
+  it 'displays pop-up when admin clicks "Approve Final Submission"' do
+    accept_confirm do
+      click_button 'Approve Final Submission'
+    end
+    expect(page.current_path).to eq(admin_submissions_index_path(submission.degree.degree_type, :final_submission_submitted))
+  end
+end
