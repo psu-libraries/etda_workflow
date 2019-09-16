5e4462660a429a9103205969aa83239e8223add7
Alex Kiessling <ajkiessl@gmail.com>
2019-09-16 15:58:48 -0400

    ETDA-1539 fixed spec.

--- spec/models/ldap_search_filter_spec.rb
+++ spec/models/ldap_search_filter_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe LdapSearchFilter, type: :model do
   describe '#create_filter' do
     let(:ldap_faculty) { Net::LDAP::Filter.eq('edupersonprimaryaffiliation', "FACULTY") }
     let(:ldap_staff) { Net::LDAP::Filter.eq('edupersonprimaryaffiliation', "STAFF") }
+    let(:ldap_emeritus) { Net::LDAP::Filter.eq('edupersonprimaryaffiliation', "EMERITUS") }
+    let(:ldap_retired) { Net::LDAP::Filter.eq('edupersonprimaryaffiliation', "RETIRED") }
 
     context 'search for first and last name' do
       let(:ldap_search) { Net::LDAP::Filter.eq('cn', 'jim* smith*') }
-      let(:ldap_filter) { Net::LDAP::Filter.join(Net::LDAP::Filter.intersect(ldap_faculty, ldap_staff), ldap_search) }
+      let(:ldap_combined) { Net::LDAP::Filter.intersect(Net::LDAP::Filter.intersect(ldap_faculty, ldap_staff), Net::LDAP::Filter.intersect(ldap_emeritus, ldap_retired)) }
+      let(:ldap_filter) { Net::LDAP::Filter.join(ldap_combined, ldap_search) }
 
       it 'returns the same filter' do
         filter = described_class.new('jim smith', true).create_filter
@@ -21,7 +24,8 @@ RSpec.describe LdapSearchFilter, type: :model do
 
     context 'search with only first name' do
       let(:ldap_search) { Net::LDAP::Filter.eq('sn', 'lee*') }
-      let(:ldap_filter) { Net::LDAP::Filter.join(Net::LDAP::Filter.intersect(ldap_faculty, ldap_staff), ldap_search) }
+      let(:ldap_combined) { Net::LDAP::Filter.intersect(Net::LDAP::Filter.intersect(ldap_faculty, ldap_staff), Net::LDAP::Filter.intersect(ldap_emeritus, ldap_retired)) }
+      let(:ldap_filter) { Net::LDAP::Filter.join(ldap_combined, ldap_search) }
 
       it 'returns the same filter' do
         filter = described_class.new('lee ', true).create_filter
@@ -35,7 +39,8 @@ RSpec.describe LdapSearchFilter, type: :model do
 
     context 'search with three names' do
       let(:ldap_search) { Net::LDAP::Filter.eq('cn', 'lee harvey oswald*') }
-      let(:ldap_filter) { Net::LDAP::Filter.join(Net::LDAP::Filter.intersect(ldap_faculty, ldap_staff), ldap_search) }
+      let(:ldap_combined) { Net::LDAP::Filter.intersect(Net::LDAP::Filter.intersect(ldap_faculty, ldap_staff), Net::LDAP::Filter.intersect(ldap_emeritus, ldap_retired)) }
+      let(:ldap_filter) { Net::LDAP::Filter.join(ldap_combined, ldap_search) }
 
       it 'returns the same filter' do
         filter = described_class.new('lee harvey oswald', true).create_filter
