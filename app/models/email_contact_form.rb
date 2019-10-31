3d54c0551a8c431142fd14d6406da7c3dd2db6e9
Alex Kiessling <ajkiessl@gmail.com>
2019-10-31 11:25:08 -0400

    ETDA-1350 Interface all set up.

--- app/models/email_contact_form.rb
+++ app/models/email_contact_form.rb
@@ -4,7 +4,12 @@ class EmailContactForm < MailForm::Base
   attribute :psu_id,         validate: true, allow_blank: true
   attribute :desc,           validate: true
   attribute :message,        validate: true
+  attribute :issue_type,     validate: true
   attribute :nickname,       captcha: true
+
+  def tooltip_message
+    "<strong>General formatting/data issues:</strong> Your email will be directed to The #{current_partner.name}.  If you are unsure about what data to input, the next steps to take, or need data to be changed by an administrator, please select this option.<br/><br/><strong>Failures to upload, access, or submit:</strong>  Your email will be directed to IT support staff.  If you are encountering website issues, having trouble accessing pages, not seeing data that should be displayed, or any other technical issues, please select this option.".html_safe
+  end
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
