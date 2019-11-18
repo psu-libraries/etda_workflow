5dce49cd20e4dd0c8879bb541112a425a2574259
Alex Kiessling <ajkiessl@gmail.com>
2019-11-18 12:49:18 -0500

    ETDA-1594 config change.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,7 +5,9 @@ sidekiq_config = Hash.new
 
 sidekiq_config['password'] = redis_config['password'] if redis_config['password']
 sidekiq_config['namespace'] = "etda_workflow_#{current_partner.id}"
-sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/0"
+sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/0" if current_partner.graduate?
+sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/1" if current_partner.honors?
+sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/2" if current_partner.milsch?
 sidekiq_config['url'] = sidekiq_url
 
 Sidekiq.configure_server do |config|
