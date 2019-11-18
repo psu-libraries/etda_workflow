8fbd750cade91863237acbae55e3783e995b099c
Alex Kiessling <ajkiessl@gmail.com>
2019-11-18 11:51:08 -0500

    ETDA-1594 Alternate sidekiq configuration.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,10 @@ redis_config = Rails.application.config_for(:redis)
 sidekiq_config = Hash.new
 
 sidekiq_config['password'] = redis_config['password'] if redis_config['password']
-sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}"
+sidekiq_config['db'] = 0 if current_partner.graduate?
+sidekiq_config['db'] = 1 if current_partner.honors?
+sidekiq_config['db'] = 2 if current_partner.milsch?
+sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/#{sidekiq_config['db']}"
 sidekiq_config['url'] = sidekiq_url
 
 Sidekiq.configure_server do |config|
