6bfe30a7f3a604fc07e74d1b188620fa93c734d0
Alex Kiessling <ajkiessl@gmail.com>
2019-11-18 12:43:59 -0500

    ETDA-1594 New config.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,76 +1,17 @@
-#
-# redis_config = Rails.application.config_for(:redis)
-#
-# sidekiq_config = Hash.new
-#
-# sidekiq_config['password'] = redis_config['password'] if redis_config['password']
-# sidekiq_config['db'] = 0 if current_partner.graduate?
-# sidekiq_config['db'] = 1 if current_partner.honors?
-# sidekiq_config['db'] = 2 if current_partner.milsch?
-# sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/#{sidekiq_config['db']}"
-# sidekiq_config['url'] = sidekiq_url
-#
-# Sidekiq.configure_server do |config|
-#   config.redis = sidekiq_config
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = sidekiq_config
-# end
+
 redis_config = Rails.application.config_for(:redis)
+
 sidekiq_config = Hash.new
+
 sidekiq_config['password'] = redis_config['password'] if redis_config['password']
+sidekiq_config['namespace'] = "etda_workflow_#{current_partner.id}"
+sidekiq_url = "redis://#{redis_config.fetch('host', 'localhost')}:#{redis_config.fetch('port', 6379)}/0"
+sidekiq_config['url'] = sidekiq_url
 
-if current_partner.graduate?
-  Sidekiq.configure_server do |config|
-    config.redis = {
-        url: 'redis://127.0.0.1:6379/1',
-        db: 1,
-        password: sidekiq_config['password']
-    }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {
-        url: 'redis://127.0.0.1:6379/1',
-        db: 1,
-        password: sidekiq_config['password']
-    }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
 end
 
-if current_partner.honors?
-  Sidekiq.configure_server do |config|
-    config.redis = {
-        url: 'redis://127.0.0.1:6379/2',
-        db: 2,
-        password: sidekiq_config['password']
-    }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {
-        url: 'redis://127.0.0.1:6379/2',
-        db: 2,
-        password: sidekiq_config['password']
-    }
-  end
-end
-
-if current_partner.milsch?
-  Sidekiq.configure_server do |config|
-    config.redis = {
-        url: 'redis://127.0.0.1:6379/3',
-        db: 3,
-        password: sidekiq_config['password']
-    }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = {
-        url: 'redis://127.0.0.1:6379/3',
-        db: 3,
-        password: sidekiq_config['password']
-    }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
 end
