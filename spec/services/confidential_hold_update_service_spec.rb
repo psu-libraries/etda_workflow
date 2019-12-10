7617a0ff964a1f2d4eb0c367d64d96d3b1c990c6
Alex Kiessling <ajkiessl@gmail.com>
2019-12-10 14:11:56 -0500

    ETDA-1487 Rubocop fixes.

--- spec/services/confidential_hold_update_service_spec.rb
+++ spec/services/confidential_hold_update_service_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe ConfidentialHoldUpdateService do
 
   context 'when login controller updates' do
     it 'sets confidential hold and history' do
-      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return( { confidential_hold: true } )
-      conf_hold_update_service = ConfidentialHoldUpdateService.new(author, 'login_controller')
+      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return(confidential_hold: true)
+      conf_hold_update_service = described_class.new(author, 'login_controller')
       conf_hold_update_service.update
       expect(Author.find(author.id).confidential_hold).to eq true
       expect(Author.find(author.id).confidential_hold_set_at).to be_present
@@ -23,8 +23,8 @@ RSpec.describe ConfidentialHoldUpdateService do
     it 'removes confidential hold and sets history' do
       author.update_attributes confidential_hold: true, confidential_hold_set_at: DateTime.now
       FactoryBot.create :confidential_hold_history, author: author
-      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return( { confidential_hold: false } )
-      conf_hold_update_service = ConfidentialHoldUpdateService.new(author, 'login_controller')
+      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return(confidential_hold: false)
+      conf_hold_update_service = described_class.new(author, 'login_controller')
       conf_hold_update_service.update
       expect(Author.find(author.id).confidential_hold).to eq false
       expect(Author.find(author.id).confidential_hold_set_at).not_to be_present
@@ -37,8 +37,8 @@ RSpec.describe ConfidentialHoldUpdateService do
 
     it 'adds another history' do
       FactoryBot.create :confidential_hold_history, author: author, removed_at: DateTime.now, removed_by: 'login_controller'
-      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return( { confidential_hold: true } )
-      conf_hold_update_service = ConfidentialHoldUpdateService.new(author, 'login_controller')
+      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return(confidential_hold: true)
+      conf_hold_update_service = described_class.new(author, 'login_controller')
       conf_hold_update_service.update
       expect(Author.find(author.id).confidential_hold).to eq true
       expect(Author.find(author.id).confidential_hold_set_at).to be_present
@@ -49,8 +49,8 @@ RSpec.describe ConfidentialHoldUpdateService do
       author.update_attributes confidential_hold: true, confidential_hold_set_at: DateTime.now
       FactoryBot.create :confidential_hold_history, author: author, removed_at: DateTime.now, removed_by: 'login_controller'
       FactoryBot.create :confidential_hold_history, author: author
-      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return( { confidential_hold: false } )
-      conf_hold_update_service = ConfidentialHoldUpdateService.new(author, 'login_controller')
+      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return(confidential_hold: false)
+      conf_hold_update_service = described_class.new(author, 'login_controller')
       conf_hold_update_service.update
       expect(Author.find(author.id).confidential_hold_histories.count).to eq 2
       expect(Author.find(author.id).confidential_hold_histories.last.removed_at).to be_present
@@ -60,8 +60,8 @@ RSpec.describe ConfidentialHoldUpdateService do
 
   context 'when daily report updates' do
     it 'updates confidential hold history with daily report metadata' do
-      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return( { confidential_hold: true } )
-      conf_hold_update_service = ConfidentialHoldUpdateService.new(author, 'daily_report')
+      allow_any_instance_of(LdapUniversityDirectory).to receive(:retrieve).and_return(confidential_hold: true)
+      conf_hold_update_service = described_class.new(author, 'daily_report')
       conf_hold_update_service.update
       expect(Author.find(author.id).confidential_hold_histories.first.set_by).to eq 'daily_report'
     end
