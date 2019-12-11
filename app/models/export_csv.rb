7a33aa9b987fb5de9f039cdad0fe0cc1f385db7b
Alex Kiessling <ajkiessl@gmail.com>
2019-12-11 08:48:57 -0500

    ETDA-1487 CSV export workingish? Going to test on server and download from other os to see if it works.

--- app/models/export_csv.rb
+++ app/models/export_csv.rb
@@ -1,10 +1,10 @@
 class ExportCsv
   attr_accessor :query_type
-  attr_reader :submissions
+  attr_reader :records
 
-  def initialize(query_type, submissions)
+  def initialize(query_type, records)
     @query_type = query_type
-    @submissions = submissions
+    @records = records
   end
 
   def columns
@@ -17,16 +17,18 @@ class ExportCsv
     when 'custom_report'
       column_list = ['Last Name', 'First Name', 'Id', 'Title', 'Degree', 'Access Level', 'Date', 'Status']
       column_list.append('Invention Disclosure Number')
+    when 'confidential_hold_report'
+      column_list = ['ID', 'Last Name', 'First Name', 'Middle Name', 'Access ID', 'PSU Email Address', 'Alternate Email Address', 'PSU ID', 'Confidential Hold', 'Confidential Hold Set At']
     else
       column_list = nil
     end
     column_list
   end
 
-  def fields(submission)
-    return nil if submission.nil?
+  def fields(record)
+    return nil if record.nil?
 
-    s = submission
+    s = record
 
     case query_type
     when 'final_submission_approved'
@@ -37,6 +39,8 @@ class ExportCsv
     when 'custom_report'
       field_list = [s.author.last_name, s.author.first_name, s.id, s.cleaned_title, s.degree_type.name, s.current_access_level.label, s.semester_and_year, s.status]
       #      field_list.append(s.invention_disclosure.first.id_number) if current_partner.graduate? && !s.invention_disclosure.nil?
+    when 'confidential_hold_report'
+      field_list = [s.author.id, s.author.last_name, s.author.first_name, s.author.middle_name, s.author.access_id, s.author.psu_email_address, s.author.alternate_email_address, s.author.psu_idn, s.author.confidential_hold, s.author.confidential_hold_set_at]
     else
       field_list = nil
     end
---
+++ app/views/admin/reports/confidential_hold_report_index.json.rabl
@@ -0,0 +1,18 @@
+object false
+
+node(:data) do
+  table = @authors.map do |author|
+    row = [
+      "<input type='checkbox' class='row-checkbox' />",
+      author.last_name,
+      author.first_name,
+      author.middle_name,
+      author.access_id,
+      author.psu_email_address,
+      author.alternate_email_address,
+      author.psu_idn,
+      author.confidential_hold,
+      author.confidential_hold_set_at
+    ]
+  end
+end
