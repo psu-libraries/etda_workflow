9f9b435f27f2aba38ed924a58c119fd68a201777
Alex Kiessling <ajkiessl@gmail.com>
2020-01-23 14:55:24 -0500

    ETDA-1652 removed pop ups for millennium explore.

--- spec/models/etd_urls_spec.rb
+++ spec/models/etd_urls_spec.rb
@@ -24,13 +24,4 @@ RSpec.describe EtdUrls, type: :model do
       expect(described_class.new.workflow).to eql('https://submit-millennium-scholars-.libraries.psu.edu.localhost:3000') if current_partner.milsch?
     end
   end
-
-  context '#popup' do
-    it 'graduate and honors are blank' do
-      expect(described_class.new.popup).to be_blank unless current_partner.milsch?
-    end
-    it 'returns an alert for millennium scholars' do
-      expect(described_class.new.popup).to eql("alert('Millennium Scholars Explore Coming Soon'); return false;") if current_partner.milsch?
-    end
-  end
 end
