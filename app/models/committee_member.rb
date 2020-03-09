1e445bcc2782a78effec2b66d518891c1b014e8d
Alex Kiessling <ajkiessl@gmail.com>
2020-03-09 17:08:36 -0400

    ETDA-1679 all_have_voted now includes non voting members(except head_of_program).  Refactored a bit.

--- app/models/committee_member.rb
+++ app/models/committee_member.rb
@@ -43,7 +43,8 @@ class CommitteeMember < ApplicationRecord
 
   def self.head_of_program(submission_id)
     @submission = Submission.find(submission_id)
-    CommitteeMember.where(submission: @submission).find_by(committee_role: CommitteeRole.find_by(name: 'Program Head/Chair', degree_type: @submission.degree.degree_type))
+    program_head_role = CommitteeRole.find_by(name: 'Program Head/Chair', degree_type: @submission.degree.degree_type)
+    CommitteeMember.where(submission: @submission).find_by(committee_role: program_head_role)
   end
 
   def validate_email
