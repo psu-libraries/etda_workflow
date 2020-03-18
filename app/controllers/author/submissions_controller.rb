aa2c5bd49ab08c1b1a0e633437c013c578ff1434
Alex Kiessling <ajkiessl@gmail.com>
2020-03-18 11:45:59 -0400

    ETDA-1665 Moved author submit final submission logic into FinalSubmissionSubmitService.

--- app/controllers/author/submissions_controller.rb
+++ app/controllers/author/submissions_controller.rb
@@ -116,42 +116,10 @@ class Author::SubmissionsController < AuthorController
     @submission = find_submission
     approval_status = ApprovalStatus.new(@submission).status
     status_giver = SubmissionStatusGiver.new(@submission)
-    status_giver.can_upload_final_submission_files?
-    @submission.update_attributes!(final_submission_params)
-    @submission.update_attribute :publication_release_terms_agreed_to_at, Time.zone.now
-    if @submission.status == 'waiting for committee review rejected'
-      current_partner.honors? ? status_giver.can_waiting_for_committee_review? : status_giver.can_waiting_for_final_submission?
-      current_partner.honors? ? status_giver.waiting_for_committee_review! : status_giver.waiting_for_final_submission_response!
-      OutboundLionPathRecord.new(submission: @submission).report_status_change
-      @submission.reset_committee_reviews
-      @submission.update_final_submission_timestamps!(Time.zone.now)
-      redirect_to author_root_path
-      WorkflowMailer.send_final_submission_received_email(@submission)
-      flash[:notice] = 'Final submission files uploaded successfully.'
-      return
-    elsif @submission.status == 'collecting final submission files rejected' && current_partner.honors?
-      status_giver.can_waiting_for_final_submission?
-      status_giver.waiting_for_final_submission_response!
-      OutboundLionPathRecord.new(submission: @submission).report_status_change
-      @submission.update_final_submission_timestamps!(Time.zone.now)
-      redirect_to author_root_path
-      WorkflowMailer.send_final_submission_received_email(@submission)
-      flash[:notice] = 'Final submission files uploaded successfully.'
-      return
-    end
-    if current_partner.honors?
-      status_giver.can_waiting_for_committee_review?
-      status_giver.waiting_for_committee_review!
-      @submission.reset_committee_reviews
-      @submission.committee_review_requests_init unless approval_status == 'approved'
-    else
-      status_giver.can_waiting_for_final_submission?
-      status_giver.waiting_for_final_submission_response!
-    end
-    OutboundLionPathRecord.new(submission: @submission).report_status_change
-    @submission.update_final_submission_timestamps!(Time.zone.now)
+    submit_service = FinalSubmissionSubmitService.new(@submission, status_giver,
+                                                      approval_status, final_submission_params)
+    submit_service.submit_final_submission
     redirect_to author_root_path
-    WorkflowMailer.send_final_submission_received_email(@submission)
     flash[:notice] = 'Final submission files uploaded successfully.'
   rescue ActiveRecord::RecordInvalid
     @view = Author::FinalSubmissionFilesView.new(@submission)
---
+++ spec/integration/author/submit_final_submission_spec.rb
@@ -0,0 +1,145 @@
+RSpec.describe 'Submitting a final submission as an author', js: true do
+  require 'integration/integration_spec_helper'
+
+  describe "When collecting final submission files" do
+    before do
+      webaccess_authorize_author
+    end
+
+    let!(:author) { current_author }
+    let!(:submission) { FactoryBot.create :submission, :collecting_final_submission_files, lion_path_degree_code: 'PHD', author: author, degree: degree }
+    let!(:inbound_record) { FactoryBot.create :inbound_lion_path_record, author: author }
+    let!(:committee_members) { create_committee(submission) }
+    let!(:degree) { FactoryBot.create :degree, degree_type: DegreeType.default }
+    let!(:approval_configuration) { FactoryBot.create :approval_configuration, degree_type: degree.degree_type, head_of_program_is_approving: false }
+
+    context "when I submit the 'Upload Final Submission Files' form" do
+      it 'loads the page' do
+        submission.defended_at = Time.zone.yesterday
+        submission.save(validate: false)
+        submission.reload
+        expect(submission.final_submission_files_uploaded_at).to be_nil
+        visit author_submission_edit_final_submission_path(submission)
+        expect(page).to have_content('Select one or more files to upload')
+        expect(page).not_to have_link('Contact Support')
+        select Time.zone.now.next_year.year, from: 'Graduation Year'
+        select 'Spring', from: 'Semester Intending to Graduate'
+        fill_in 'Abstract', with: 'A paper on stuff'
+        page.find(".submission_delimited_keywords .ui-autocomplete-input").set('stuff')
+        choose "submission_access_level_open_access" if current_partner.graduate?
+        expect(page).to have_css('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')
+        first_input_id = first('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')[:id]
+        attach_file first_input_id, fixture('final_submission_file_01.pdf')
+        find("#submission_federal_funding_false").click
+        expect(page).to have_content('I hereby certify that')
+        check 'I agree to copyright statement'
+        click_button 'Submit final files for review'
+        # expect(page).to have_content('successfully')
+        submission.reload
+        expect(submission.status).to eq 'waiting for final submission response' unless current_partner.honors?
+        expect(submission.status).to eq 'waiting for committee review' if current_partner.honors?
+        submission.reload
+        expect(submission.federal_funding).to eq false
+        expect(submission.final_submission_files_uploaded_at).not_to be_nil
+        expect(WorkflowMailer.deliveries.count).to eq(1) unless current_partner.honors?
+        expect(WorkflowMailer.deliveries.count).to eq(3) if current_partner.honors?
+      end
+    end
+
+    context "when I submit the 'Upload Final Submission Files' form after committee rejection" do
+      it 'proceeds to "waiting for final submission response" and resets committee reviews' do
+        submission.committee_members.first.update_attribute :status, 'rejected'
+        submission.status = 'waiting for committee review rejected'
+        submission.defended_at = Time.zone.yesterday
+        submission.save(validate: false)
+        submission.reload
+        visit author_submission_edit_final_submission_path(submission)
+        select Time.zone.now.next_year.year, from: 'Graduation Year'
+        select 'Spring', from: 'Semester Intending to Graduate'
+        fill_in 'Abstract', with: 'A paper on stuff'
+        page.find(".submission_delimited_keywords .ui-autocomplete-input").set('stuff')
+        choose "submission_access_level_open_access" if current_partner.graduate?
+        expect(page).to have_css('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')
+        first_input_id = first('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')[:id]
+        attach_file first_input_id, fixture('final_submission_file_01.pdf')
+        check 'I agree to copyright statement'
+        click_button 'Submit final files for review'
+        # expect(page).to have_content('successfully')
+        submission.reload
+        expect(page).to have_current_path(author_root_path)
+        expect(submission.committee_members.first.status).to eq ''
+        expect(submission.status).to eq 'waiting for final submission response' unless current_partner.honors?
+        expect(submission.status).to eq 'waiting for committee review' if current_partner.honors?
+        expect(submission.final_submission_files_uploaded_at).not_to be_nil
+        expect(WorkflowMailer.deliveries.count).to eq(1)
+      end
+    end
+
+    context "when I submit the 'Upload Final Submission Files' form with multiple files" do
+      it 'uploads two files' do
+        submission.defended_at = Time.zone.yesterday
+        submission.save(validate: false)
+        submission.reload
+        expect(submission.final_submission_files_uploaded_at).to be_nil
+        visit author_submission_edit_final_submission_path(submission)
+        expect(page).to have_content('Select one or more files to upload')
+        expect(page).not_to have_link('Contact Support')
+        select Time.zone.now.next_year.year, from: 'Graduation Year'
+        select 'Spring', from: 'Semester Intending to Graduate'
+        fill_in 'Abstract', with: 'A paper on stuff'
+        page.find(".submission_delimited_keywords .ui-autocomplete-input").set('stuff')
+        choose "submission_access_level_open_access" if current_partner.graduate?
+        expect(page).to have_css('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')
+        first_input_id = first('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')[:id]
+        attach_file first_input_id, fixture('final_submission_file_01.pdf')
+        click_link "Additional File"
+        all('input[type="file"]').last.set(fixture('final_submission_file_01.pdf'))
+        expect(page).to have_content('I hereby certify that')
+        check 'I agree to copyright statement'
+        click_button 'Submit final files for review'
+        # expect(page).to have_content('successfully')
+        submission.reload
+        expect(submission.status).to eq 'waiting for final submission response' unless current_partner.honors?
+        expect(submission.status).to eq 'waiting for committee review' if current_partner.honors?
+        expect(submission.final_submission_files_uploaded_at).not_to be_nil
+        expect(submission.final_submission_files.count).to eq(2)
+        visit "/author/submissions/#{submission.id}/final_submission"
+        expect(page).to have_link('final_submission_file_01.pdf')
+        expect(page).to have_link('final_submission_file_01.pdf')
+      end
+    end
+
+    context "when I have a legacy format review record and submit 'Upload Final Submission Files' form" do
+      it 'loads the page' do
+        submission.year = Time.zone.now.year.to_s
+        submission.semester = 'Spring'
+        submission.title = nil
+        submission.format_review_legacy_id = 99
+        submission.defended_at = Time.zone.yesterday
+        submission.save(validate: false)
+        expect(submission.final_submission_files_uploaded_at).to be_nil
+        visit author_submission_edit_final_submission_path(submission)
+        expect(page).to have_css '#submission_year'
+        expect(page).to have_css '#submission_semester'
+        expect(page).to have_css '#submission_title'
+        fill_in 'Title', with: 'This has a format review legacy record'
+        select Time.zone.now.next_year.year.to_s, from: 'submission[year]'
+        select 'Spring', from: 'submission[semester]'
+        fill_in 'Abstract', with: 'A paper on stuff'
+        page.find(".submission_delimited_keywords .ui-autocomplete-input").set('stuff')
+        choose "submission_access_level_open_access" if current_partner.graduate?
+        expect(page).to have_css('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')
+        first_input_id = first('#final-submission-file-fields .nested-fields div.form-group div:first-child input[type="file"]')[:id]
+        attach_file first_input_id, fixture('final_submission_file_01.pdf')
+        check 'I agree to copyright statement'
+        click_button 'Submit final files for review'
+        # expect(page).to have_content('successfully')
+        submission.reload
+        expect(submission.status).to eq 'waiting for final submission response' unless current_partner.honors?
+        expect(submission.status).to eq 'waiting for committee review' if current_partner.honors?
+        submission.reload
+        expect(submission.final_submission_files_uploaded_at).not_to be_nil
+      end
+    end
+  end
+end
