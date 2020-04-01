4f2aa1846866dd49086880b7e3db3e3a35b1e434
Alex Kiessling <ajkiessl@gmail.com>
2020-04-01 14:30:30 -0400

    ETDA-1698 Truncating title links and file links in datatables so they do not overflow the div.  Removed submission status from final submission column fron final submission is pending index.

--- app/presenters/admin/submission_view.rb
+++ app/presenters/admin/submission_view.rb
@@ -32,9 +32,17 @@ class Admin::SubmissionView < SimpleDelegator
 
   def most_relevant_file_links
     if final_submission_files.any?
-      final_submission_files.map { |f| context.link_to_if !f.asset_identifier.nil?, f.asset_identifier, context.admin_final_submission_file_path(f.id), 'data-no-turbolink': true }
+      final_submission_files.map do |f|
+        context.link_to_if !f.asset_identifier.nil?,
+                           f.asset_identifier.truncate(30, omission: "...#{f.asset_identifier[-7..]}"),
+                           context.admin_final_submission_file_path(f.id), 'data-no-turbolink': true
+      end
     elsif format_review_files.any?
-      format_review_files.map { |f| context.link_to_if !f.asset_identifier.nil?, f.asset_identifier, context.admin_format_review_file_path(f.id), 'data-no-turbolink': true }
+      format_review_files.map do |f|
+        context.link_to_if !f.asset_identifier.nil?,
+                           f.asset_identifier.truncate(30, omission: "...#{f.asset_identifier[-7..]}"),
+                           context.admin_format_review_file_path(f.id), 'data-no-turbolink': true
+      end
     else
       []
     end
--- app/views/admin/submissions/table_bodies/_final_submission_submitted.json.jbuilder
+++ app/views/admin/submissions/table_bodies/_final_submission_submitted.json.jbuilder
@@ -1,7 +1,7 @@
 json.array! [
   submission_view.id,
   "<input type='checkbox' class='row-checkbox' />",
-  link_to_if(submission_view.admin_can_edit?, submission_view.title.presence || '[Title not available]', admin_edit_submission_path(submission_view)),
+  link_to_if(submission_view.admin_can_edit?, submission_view.title.presence.truncate(30) || '[Title not available]', admin_edit_submission_path(submission_view)),
   submission_view.author.last_name,
   submission_view.author.first_name,
   submission_view.semester_and_year.presence || 'Date unknown',
