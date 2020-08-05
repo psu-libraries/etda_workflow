583d1554cbdc4511bb9d2fcdac0ee262bf7c4e8e
Alex Kiessling <ajkiessl@gmail.com>
2020-08-05 16:20:33 -0400

    ETDA-1768 Cleaned up and removed bugs from lionpath committee factory.  Lionpath committee basic tests for logic.

--- app/models/lionpath/lionpath_committee_factory.rb
+++ app/models/lionpath/lionpath_committee_factory.rb
@@ -8,119 +8,70 @@ class Lionpath::LionpathCommitteeFactory
   def create_member
     case row['Role']
     when 'CHMJ', 'H', 'C', 'CIMN', 'CIRA', 'CIMJ'
-      variables = {
-        committee_role: get_committee_role('Committee Chair/Co-Chair'),
-        is_required: true,
-        is_voting: true
-      }
+      variables = assign_variables('Committee Chair/Co-Chair', true)
+    when 'CMMJ', 'CMMN', 'G', 'MN', 'M', 'N'
+      variables = assign_variables('Committee Member', true)
+    when 'UF', 'GF', 'GFU', 'GU', 'F'
+      variables = assign_variables('Outside Member', true)
+    when 'UFN', 'UN', 'U', 'UFN', 'NF'
+      variables = assign_variables('Outside Member', false)
+    when 'MAMJ', 'MARA', 'MASP', 'MAGS', 'MAMN', 'CMRA'
+      variables = assign_variables('Committee Member', false)
+    when 'S'
+      variables = assign_variables('Special Member', false)
     when 'CISP'
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Chair/Co-Chair'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Special Member'),
-                               is_required: false,
-                               is_voting: true }.merge(defaults))
+      CommitteeMember.create(assign_variables('Committee Chair/Co-Chair', true).merge(defaults))
+      CommitteeMember.create(assign_variables('Special Member', false).merge(defaults))
       return
-    when 'HD'
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Chair/Co-Chair'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Dissertation Advisor/Co-Advisor'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
+    when 'HD', 'CD'
+      CommitteeMember.create(assign_variables('Committee Chair/Co-Chair', true).merge(defaults))
+      CommitteeMember.create(assign_variables('Dissertation Advisor/Co-Advisor', true).merge(defaults))
       return
     when 'HF', 'HU'
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Chair/Co-Chair'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Outside Member'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
+      CommitteeMember.create(assign_variables('Committee Chair/Co-Chair', true).merge(defaults))
+      CommitteeMember.create(assign_variables('Outside Member', true).merge(defaults))
       return
     when 'HM'
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Chair/Co-Chair'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Member'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
+      CommitteeMember.create(assign_variables('Committee Chair/Co-Chair', true).merge(defaults))
+      CommitteeMember.create(assign_variables('Committee Member', true).merge(defaults))
       return
-    when 'CMMJ', 'CMMN', 'G', 'MN', 'M', 'N'
-      variables = {
-        committee_role: get_committee_role('Committee Member'),
-        is_required: true,
-        is_voting: true
-      }
-    when 'UF', 'GF', 'GFU', 'GU', 'F'
-      variables = {
-        committee_role: get_committee_role('Outside Member'),
-        is_required: true,
-        is_voting: true
-      }
-    when 'UFN', 'UN', 'U', 'UFN', 'NF'
-      variables = {
-        committee_role: get_committee_role('Outside Member'),
-        is_required: false,
-        is_voting: true
-      }
-    when 'MAMJ', 'MARA', 'MASP', 'MAGS', 'MAMN'
-      variables = {
-        committee_role: get_committee_role('Committee Member'),
-        is_required: false,
-        is_voting: true
-      }
-    when 'CMRA'
-      variables = {
-        committee_role: get_committee_role('Committee Member'),
-        is_required: false,
-        is_voting: true
-      }
     when 'CMSP'
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Member'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Special Member'),
-                               is_required: false,
-                               is_voting: true }.merge(defaults))
+      CommitteeMember.create(assign_variables('Committee Member', true).merge(defaults))
+      CommitteeMember.create(assign_variables('Special Member', false).merge(defaults))
       return
     when 'MD'
-      CommitteeMember.create({ committee_role: get_committee_role('Committee Member'),
-                               is_required: true,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Dissertation Advisor/Co-Advisor'),
-                               is_required: false,
-                               is_voting: true }.merge(defaults))
+      CommitteeMember.create(assign_variables('Committee Member', true).merge(defaults))
+      CommitteeMember.create(assign_variables('Dissertation Advisor/Co-Advisor', false).merge(defaults))
       return
     when 'SR'
-      CommitteeMember.create({ committee_role: get_committee_role('Special Member'),
-                               is_required: false,
-                               is_voting: true }.merge(defaults))
-      CommitteeMember.create({ committee_role: get_committee_role('Dissertation Advisor/Co-Advisor'),
-                               is_required: false,
-                               is_voting: true }.merge(defaults))
+      CommitteeMember.create(assign_variables('Special Member', false).merge(defaults))
+      CommitteeMember.create(assign_variables('Dissertation Advisor/Co-Advisor', false).merge(defaults))
       return
-    when 'S'
-      variables = {
-        committee_role: get_committee_role('Special Member'),
-        is_required: false,
-        is_voting: true
-      }
     end
-    CommitteeMember.create defaults.merge(variables)
+    CommitteeMember.create variables.merge(defaults)
   end
 
   private
 
+  def assign_variables(committee_role, is_required)
+    {
+      committee_role: get_committee_role(committee_role),
+      is_required: is_required
+    }
+  end
+
   def get_committee_role(role_name)
-    sub_roles.select { |r| r.name == role_name }
+    sub_roles.find { |r| r.name == role_name }
   end
 
   def defaults
     {
       name: "#{row['First Name']} #{row['Last Name']}",
-      email: "#{row['Access ID'].downcase}.psu.edu",
+      email: "#{row['Access ID'].downcase}@psu.edu",
       access_id: row['Access ID'].downcase.to_s,
-      submission: submission
+      submission: submission,
+      lionpath_uploaded_at: DateTime.now,
+      is_voting: true
     }
   end
 
