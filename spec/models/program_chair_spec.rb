591921adb4f489260060b36655e50750b5ef9d2d
Alex Kiessling <ajkiessl@gmail.com>
2020-12-03 12:28:09 -0500

    ETDA-1808 changed/added timestamps to indicate last time the record was updated by lionpath import, rather than a single import timestamp.

--- spec/models/program_chair_spec.rb
+++ spec/models/program_chair_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe ProgramChair, type: :model do
   it { is_expected.to have_db_column(:campus).of_type(:string) }
   it { is_expected.to have_db_column(:phone).of_type(:integer) }
   it { is_expected.to have_db_column(:email).of_type(:string) }
+  it { is_expected.to have_db_column(:lionpath_updated_at).of_type(:datetime) }
 
   it { is_expected.to belong_to :program }
 end
