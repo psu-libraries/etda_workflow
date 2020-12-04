855a7359f499614db9958c690d1dd175f80aaa9b
Alex Kiessling <ajkiessl@gmail.com>
2020-12-04 09:49:12 -0500

    Logic to use different sftp host/id for etdaworkflow1prod vs etdaworkflow1dev

--- bin/lionpath-chair.sh
+++ bin/lionpath-chair.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Using SFTP connection with Lionpath host to pull file names in order of most to least recent
-OUTPUT=$(sftp -P 22 -b bin/lp_sftp_newest.bat -i ~/.ssh/id_rsa_lionpath_prod uldsrdc@prod-nfs.lionpath.psu.edu)
+if [ $HOSTNAME = 'etdaworkflow1prod' ]; then
+  OUTPUT=$(sftp -P 22 -b bin/lp_sftp_newest.bat -i ~/.ssh/id_rsa_lionpath_prod uldsrdc@prod-nfs.lionpath.psu.edu)
+else
+  OUTPUT=$(sftp -P 22 -b bin/lp_sftp_newest.bat -i ~/.ssh/id_rsa_lionpath_test uldsrdc@qna-nfs.lionpath.psu.edu)
+fi
 
 # Single out the first PE_SR_G_ETD_CHAIR_PRC and pull this down as var/tmp_lionpath/lionpath.csv
 for x in $OUTPUT
