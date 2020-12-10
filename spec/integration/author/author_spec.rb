3f206dd6073e9fa7cd5f5a1181304f77a54a1777
Alex Kiessling <ajkiessl@gmail.com>
2020-12-10 09:50:26 -0500

    ETDA-1809 alternating logic for partners in integration/author_spec .

--- spec/integration/author/author_spec.rb
+++ spec/integration/author/author_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Author submission page', type: :integration, js: true do
+RSpec.describe 'Author submission page', type: :integration, milsch: true, honors: true, js: true do
   require 'integration/integration_spec_helper'
 
   before do
@@ -21,7 +21,7 @@ RSpec.describe 'Author submission page', type: :integration, js: true do
   context 'Author submission display when author has no submissions' do
     it "displays 'no submissions message'" do
       expect(page).to have_content("You don't have any submissions")
-      expect(page).to have_content("in LionPath") if current_partner.graduate?
+      expect(page).to have_content("in LionPATH") if current_partner.graduate?
       expect(page).to have_link('Start a new Submission') unless current_partner.graduate?
       expect(page).not_to have_link('Contact Support')
     end
@@ -34,17 +34,16 @@ RSpec.describe 'Author submission page', type: :integration, js: true do
       expect(page).not_to have_content("You don't have any submissions yet.")
       expect(page).not_to have_content('Existing submission found. The status of your previously submitted document is listed below.')
       expect(page).to have_link('My Published Submissions')
-      expect(page).to have_content('If you would like to start')
+      expect(page).to have_content('If you would like to start') unless current_partner.graduate?
     end
   end
 
   context 'Author submission display when author has one submission' do
-    it "displays 'submission found'", honors: true, milsch: true do
+    it "displays 'submission found'" do
       FactoryBot.create :submission, :collecting_committee, author: author
       visit author_submissions_path
       expect(page).to have_content('Existing submission found. The status of your previously submitted document is listed below.')
       expect(page).to have_link('Provide Committee')
-      expect(page).to have_content('If you would like to start a new thesis or dissertation,') if current_partner.graduate?
       expect(page).to have_content('If you would like to start a new thesis') unless current_partner.graduate?
     end
   end
