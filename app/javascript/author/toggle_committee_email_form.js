7c388de4a732c0d2c1fc4c676d6f6567434be157
Alex Kiessling <ajkiessl@gmail.com>
2021-03-02 13:49:21 -0500

    changed style of cancel links

--- app/javascript/author/toggle_committee_email_form.js
+++ app/javascript/author/toggle_committee_email_form.js
@@ -3,7 +3,7 @@ window.jQuery = $;
 
 initialize_email_checkboxes = function() {
     $('[id=email_form_release_switch]').click(function () {
-        var i = $(this).closest('.panel').find("input[type='email']");
+        var i = $(this).closest('.card').find("input[type='email']");
         if (this.checked) {
             i.removeAttr('readonly');
             i.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
