4dcaaa85f89db39d2f3de311b2a27beebf7cda08
Alex Kiessling <ajkiessl@gmail.com>
2021-03-25 12:57:27 -0400

    ETDA-1883 - Specs for new committee page logic

--- spec/presenters/author/committee_form_view_spec.rb
+++ spec/presenters/author/committee_form_view_spec.rb
@@ -34,12 +34,93 @@ RSpec.describe Author::CommitteeFormView do
     end
   end
 
-  context '#new_committee_label' do
-    author = FactoryBot.create :author
-    submission = FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)], author: author
-    it 'returns "Add Committee Members"' do
-      view = described_class.new(submission)
-      expect(view.new_committee_label).to eq('Add Committee Members')
+  describe '#new_committee_label' do
+    let!(:degree1) { FactoryBot.create :degree, degree_type: DegreeType.default }
+    let!(:degree2) { FactoryBot.create :degree, degree_type: (FactoryBot.create :degree_type) }
+    let!(:author) { FactoryBot.create :author }
+
+    context 'when submission is a dissertation' do
+      let!(:submission) do
+        FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)],
+                          author: author, degree: degree1
+      end
+
+      it 'returns "Committee Members"' do
+        view = described_class.new(submission)
+        expect(view.new_committee_label).to eq('Committee Members')
+      end
+    end
+
+    context 'when submission is not a dissertation' do
+      let!(:submission) do
+        FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)],
+                          author: author, degree: degree2
+      end
+
+      it 'returns "Add Committee Members"' do
+        view = described_class.new(submission)
+        expect(view.new_committee_label).to eq('Add Committee Members')
+      end
+    end
+  end
+
+  describe '#update_committee_label' do
+    let!(:degree1) { FactoryBot.create :degree, degree_type: DegreeType.default }
+    let!(:degree2) { FactoryBot.create :degree, degree_type: (FactoryBot.create :degree_type) }
+    let!(:author) { FactoryBot.create :author }
+
+    context 'when submission is a dissertation' do
+      let!(:submission) do
+        FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)],
+                          author: author, degree: degree1
+      end
+
+      it 'returns "Committee Members"' do
+        view = described_class.new(submission)
+        expect(view.update_committee_label).to eq('Committee Members')
+      end
+    end
+
+    context 'when submission is not a dissertation' do
+      let!(:submission) do
+        FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)],
+                          author: author, degree: degree2
+      end
+
+      it 'returns "Update Committee Members"' do
+        view = described_class.new(submission)
+        expect(view.update_committee_label).to eq('Update Committee Members')
+      end
+    end
+  end
+
+  describe '#add_member_label' do
+    let!(:degree1) { FactoryBot.create :degree, degree_type: DegreeType.default }
+    let!(:degree2) { FactoryBot.create :degree, degree_type: (FactoryBot.create :degree_type) }
+    let!(:author) { FactoryBot.create :author }
+
+    context 'when submission is a dissertation' do
+      let!(:submission) do
+        FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)],
+                          author: author, degree: degree1
+      end
+
+      it 'returns "Add Special Signatory"' do
+        view = described_class.new(submission)
+        expect(view.add_member_label).to eq('Add Special Signatory')
+      end
+    end
+
+    context 'when submission is not a dissertation' do
+      let!(:submission) do
+        FactoryBot.create :submission, committee_members: [FactoryBot.create(:committee_member)],
+                          author: author, degree: degree2
+      end
+
+      it 'returns "Add Committee Member"' do
+        view = described_class.new(submission)
+        expect(view.add_member_label).to eq('Add Committee Member')
+      end
     end
   end
 
