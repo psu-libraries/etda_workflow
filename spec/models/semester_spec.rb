a7ebc847761175532aa9b277dcb79286cef26452
Alex Kiessling <ajkiessl@gmail.com>
2021-03-29 09:45:10 -0400

    ETDA-1846 - Spec for Semester.last

--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -4,19 +4,42 @@ require 'model_spec_helper'
 
 RSpec.describe Semester, type: :model do
   describe 'Semester' do
-    year = Time.now.year
-    it 'returns fall for dates after 8-15' do
-      semester = described_class.current(Time.zone.parse("#{year}-8-16:T00:00"))
-      expect(semester).to eql("#{year} Fall")
+    let(:year) { Time.now.year }
+
+    describe '#current' do
+      let(:semester) { described_class.current }
+
+      it 'returns fall for dates after 8-15' do
+        allow(Time).to receive_message_chain(:zone, :today).and_return(Time.zone.parse("#{year}-8-16:T00:00"))
+        expect(semester).to eql("#{year} Fall")
+      end
+      it 'returns summer for dates after 5-15' do
+        allow(Time).to receive_message_chain(:zone, :today).and_return(Time.zone.parse("#{year}-5-16:T00:00"))
+        expect(semester).to eql("#{year} Summer")
+      end
+      it 'returns spring for dates before 5-16' do
+        allow(Time).to receive_message_chain(:zone, :today).and_return(Time.zone.parse("#{year}-5-15:T00:00"))
+        expect(semester).to eql("#{year} Spring")
+      end
     end
-    it 'returns summer for dates after 5-15' do
-      semester = described_class.current(Time.zone.parse("#{year}-5-16:T00:00"))
-      expect(semester).to eql("#{year} Summer")
+
+    describe '#last' do
+      let(:semester) { described_class.last }
+
+      it 'returns Summer for dates after 8-15' do
+        allow(Time).to receive_message_chain(:zone, :today).and_return(Time.zone.parse("#{year}-8-16:T00:00"))
+        expect(semester).to eql("#{year} Summer")
+      end
+      it 'returns Spring for dates after 5-15' do
+        allow(Time).to receive_message_chain(:zone, :today).and_return(Time.zone.parse("#{year}-5-16:T00:00"))
+        expect(semester).to eql("#{year} Spring")
+      end
+      it 'returns Fall for dates before 5-16' do
+        allow(Time).to receive_message_chain(:zone, :today).and_return(Time.zone.parse("#{year}-5-15:T00:00"))
+        expect(semester).to eql("#{year - 1} Fall")
+      end
     end
-    it 'returns spring for dates before 5-16' do
-      semester = described_class.current(Time.zone.parse("#{year}-5-15:T00:00"))
-      expect(semester).to eql("#{year} Spring")
-    end
+
     it 'returns all years for admin drop-down' do
       all_years = described_class.all_years
       expect(all_years).to be_an(Array)
