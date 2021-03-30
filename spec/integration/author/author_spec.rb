149486c3cabfd0a6202d6c050704bc80f30072aa
Alex Kiessling <ajkiessl@gmail.com>
2021-03-30 16:00:08 -0400

    ETDA-1778 - Separate buton for starting a new submission for honors.  Dialog popup to confirm

--- spec/integration/author/author_spec.rb
+++ spec/integration/author/author_spec.rb
@@ -45,6 +45,8 @@ RSpec.describe 'Author submission page', type: :integration, milsch: true, honor
       expect(page).to have_content('Existing submission found. The status of your previously submitted document is listed below.')
       expect(page).to have_link('Provide Committee')
       expect(page).to have_content('If you would like to start a new thesis') unless current_partner.graduate?
+      new_link = find_link('Start a new Submission')
+      expect(new_link['outerHTML']).to match(/You already have a/)
     end
   end
 
