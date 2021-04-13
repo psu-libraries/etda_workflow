f8dd494ce2386a7d7e81c143d7498875aa9b0a57
Alex Kiessling <ajkiessl@gmail.com>
2021-04-13 16:51:46 -0400

    ETDA-1916 - Added check that only allows expired records to be deleted if they are less than 5 years old - Spec for this

--- app/models/lionpath/lionpath_delete_expired_records.rb
+++ app/models/lionpath/lionpath_delete_expired_records.rb
@@ -1,19 +1,23 @@
 class Lionpath::LionpathDeleteExpiredRecords
-
   class << self
     def delete
       lp_subs_to_delete.each(&:destroy) if safe_to_delete?(total_lp_sub_count, lp_subs_to_delete.count)
-      lp_cmtee_mmbrs_to_delete.each(&:destroy) if safe_to_delete?(total_lp_cmtee_mmbr_count, lp_cmtee_mmbrs_to_delete.count)
+      lp_cmtee_mmbrs_to_delete.each(&:destroy) if safe_to_delete?(total_lp_cmtee_mmbr_count,
+                                                                  lp_cmtee_mmbrs_to_delete.count)
     end
 
     private
 
     def total_lp_subs
-      Submission.where('submissions.lionpath_updated_at IS NOT NULL')
+      Submission
+        .where('submissions.lionpath_updated_at IS NOT NULL AND submissions.created_at > ?',
+               (DateTime.now - 5.years))
     end
 
     def total_lp_cmtee_mmbrs
-      CommitteeMember.where('committee_members.lionpath_updated_at IS NOT NULL')
+      CommitteeMember
+        .where('committee_members.lionpath_updated_at IS NOT NULL AND committee_members.created_at > ?',
+               (DateTime.now - 5.years))
     end
 
     def total_lp_sub_count
--- spec/models/lionpath/lionpath_delete_expired_records_spec.rb
+++ spec/models/lionpath/lionpath_delete_expired_records_spec.rb
@@ -7,36 +7,45 @@ RSpec.describe Lionpath::LionpathDeleteExpiredRecords do
   let!(:non_exp_lp_sub2) { FactoryBot.create :submission, lionpath_updated_at: (DateTime.now - 1.day) }
   let!(:exp_lp_sub1) { FactoryBot.create :submission, lionpath_updated_at: (DateTime.now - 2.days) }
   let!(:exp_lp_sub2) { FactoryBot.create :submission, lionpath_updated_at: (DateTime.now - 10.days) }
+  let!(:exp_lp_sub3) do
+    FactoryBot.create :submission, lionpath_updated_at: (DateTime.now - 10.days), created_at: (DateTime.now - 5.years)
+  end
   let!(:non_lp_cm1) { FactoryBot.create :committee_member }
   let!(:non_lp_cm2) { FactoryBot.create :committee_member }
   let!(:non_exp_lp_cm1) { FactoryBot.create :committee_member, lionpath_updated_at: DateTime.now }
   let!(:non_exp_lp_cm2) { FactoryBot.create :committee_member, lionpath_updated_at: (DateTime.now - 1.day) }
   let!(:exp_lp_cm1) { FactoryBot.create :committee_member, lionpath_updated_at: (DateTime.now - 2.days) }
   let!(:exp_lp_cm2) { FactoryBot.create :committee_member, lionpath_updated_at: (DateTime.now - 10.days) }
+  let!(:exp_lp_cm3) do
+    FactoryBot.create :committee_member, lionpath_updated_at: (DateTime.now - 10.days),
+                      created_at: (DateTime.now - 5.years)
+  end
 
   context "when less than 1% of the total number of records are expired" do
-    it "deletes expired lionpath submissions" do
-      200.times { FactoryBot.create :submission, lionpath_updated_at: DateTime.now }
-      expect{ described_class.delete }.to change(Submission, :count).by -2
-      expect{ exp_lp_sub1.reload }.to raise_error ActiveRecord::RecordNotFound
-      expect{ exp_lp_sub2.reload }.to raise_error ActiveRecord::RecordNotFound
+    it "deletes expired lionpath submissions that are less than 5 years old" do
+      FactoryBot.create_list :submission, 200, lionpath_updated_at: DateTime.now
+      expect { described_class.delete }.to change(Submission, :count).by(-2)
+      expect { exp_lp_sub1.reload }.to raise_error ActiveRecord::RecordNotFound
+      expect { exp_lp_sub2.reload }.to raise_error ActiveRecord::RecordNotFound
+      expect(exp_lp_sub3.reload).to eq exp_lp_sub3
     end
 
-    it "deletes expired lionpath committee_members" do
-      200.times { FactoryBot.create :committee_member, lionpath_updated_at: DateTime.now }
-      expect{ described_class.delete }.to change(CommitteeMember, :count).by -2
-      expect{ exp_lp_cm1.reload }.to raise_error ActiveRecord::RecordNotFound
-      expect{ exp_lp_cm2.reload }.to raise_error ActiveRecord::RecordNotFound
+    it "deletes expired lionpath committee_members that are less than 5 years old" do
+      FactoryBot.create_list :committee_member, 200, lionpath_updated_at: DateTime.now
+      expect { described_class.delete }.to change(CommitteeMember, :count).by(-2)
+      expect { exp_lp_cm1.reload }.to raise_error ActiveRecord::RecordNotFound
+      expect { exp_lp_cm2.reload }.to raise_error ActiveRecord::RecordNotFound
+      expect(exp_lp_cm3.reload).to eq exp_lp_cm3
     end
   end
 
   context "when more than 1% of the total number of records are expired" do
     it "does not delete expired lionpath submissions" do
-      expect{ described_class.delete }.not_to change(Submission, :count)
+      expect { described_class.delete }.not_to change(Submission, :count)
     end
 
     it "does not delete expired lionpath committee_members" do
-      expect{ described_class.delete }.not_to change(CommitteeMember, :count)
+      expect { described_class.delete }.not_to change(CommitteeMember, :count)
     end
   end
 end
