796e69eacdc632f667a231d83960f147ec10e46a
Alex Kiessling <ajkiessl@gmail.com>
2021-04-22 09:23:53 -0400

    ETDA-1930 - Lionpath submission generator class and tests

--- app/models/lionpath/lionpath_submission_generator.rb
+++ app/models/lionpath/lionpath_submission_generator.rb
@@ -1,23 +1,51 @@
 class Lionpath::LionpathSubmissionGenerator
-  attr_accessor :current_remote_user
+  attr_accessor :current_remote_user, :degree_type
 
-  def initialize(current_remote_user)
+  def initialize(current_remote_user, degree_type)
     @current_remote_user =  current_remote_user
+    @degree_type = degree_type
   end
 
-  def create_master_thesis
-    degree_type = DegreeType.find_by(slug: 'master_thesis')
-    submission = Submission.create(author: Author.find_by(access_id: current_remote_user),
-                                   program_id: Program.pluck(:id).sample,
-                                   degree_id: Degree.joins(:degree_type).where('degree_types.id = ?',
-                                                                               degree_type.id).sample.id,
-                                   year: DateTime.now.year,
-                                   semester: Semester.current.split(" ").last,
-                                   lionpath_updated_at: DateTime.now)
+  def create_submission
+    submission = Submission.create(submission_attrs)
+    dept_head = dept_head_role
+    generate_dep_head(submission, dept_head)
+    if degree_type.slug == 'dissertation'
+      rand_num = rand(1..994)
+      seq = 1
+      5.times.each do
+        com_role = CommitteeRole.where('committee_roles.degree_type_id = ? and committee_roles.code IS NOT NULL',
+                                       degree_type.id).sample
+        CommitteeMember.create(committee_role: com_role,
+                               name: "Fake Person#{rand_num + seq}",
+                               email: "abc#{rand_num + seq}@psu.edu",
+                               is_required: 1,
+                               access_id: "abc#{rand_num + seq}",
+                               is_voting: false,
+                               lionpath_updated_at: DateTime.now,
+                               submission: submission)
+        seq += 1
+      end
+    end
+  end
 
-    masters_dept_head = CommitteeRole.find_by(degree_type_id: degree_type.id, is_program_head: true)
-    rand_num = rand(1..999)
-    CommitteeMember.create(committee_role: masters_dept_head,
+  private
+
+  def submission_attrs
+    {
+      author: Author.find_by(access_id: current_remote_user),
+      program_id: Program.pluck(:id).sample,
+      degree_id: Degree.joins(:degree_type).where('degree_types.id = ?',
+                                                  degree_type.id).sample.id,
+      year: DateTime.now.year,
+      semester: Semester.current.split(" ").last,
+      lionpath_updated_at: DateTime.now
+    }
+  end
+
+  def generate_dep_head(submission, dept_head_role)
+    rand_num = rand(1..994)
+    CommitteeMember.create(committee_role: dept_head_role,
                            name: "Fake Person#{rand_num}",
                            email: "abc#{rand_num}@psu.edu",
                            is_required: 1,
@@ -27,7 +55,7 @@ class Lionpath::LionpathSubmissionGenerator
                            submission: submission)
   end
 
-  def create_dissertation
-
+  def dept_head_role
+    CommitteeRole.find_by(degree_type_id: degree_type.id, is_program_head: true)
   end
 end
--- spec/models/lionpath/lionpath_submission_generator_spec.rb
+++ spec/models/lionpath/lionpath_submission_generator_spec.rb
@@ -3,19 +3,40 @@ require 'model_spec_helper'
 RSpec.describe Lionpath::LionpathSubmissionGenerator, type: :model do
   let!(:admin_author) { FactoryBot.create :author, access_id: 'adminflow' }
 
-  it 'generates a masters thesis' do
-    FactoryBot.create :degree, degree_type: DegreeType.second
-    FactoryBot.create :program
-    expect{ described_class.new('adminflow').create_master_thesis }.to change(Submission, :count).by 1
-    expect(admin_author.submissions.first.lionpath_updated_at).to be_truthy
-    expect(admin_author.submissions.first.committee_members.count).to eq 1
-    expect(admin_author.submissions.first.committee_members.first.name).to match /Fake Person/
-    expect(admin_author.submissions.first.committee_members.first.email).to match /abc.*@psu.edu/
-    expect(admin_author.submissions.first.committee_members.first.access_id).to match /abc.*/
-    expect(admin_author.submissions.first.committee_members.first.lionpath_updated_at).to be_truthy
+  context 'when degree_type param is a masters thesis' do
+    it 'generates a masters thesis' do
+      FactoryBot.create :degree, degree_type: DegreeType.second
+      FactoryBot.create :program
+      expect{ described_class.new('adminflow', DegreeType.second).create_submission }.to change(Submission, :count).by 1
+      expect(admin_author.submissions.first.degree_type.slug).to eq 'master_thesis'
+      expect(admin_author.submissions.first.lionpath_updated_at).to be_truthy
+      expect(admin_author.submissions.first.committee_members.count).to eq 1
+      expect(admin_author.submissions.first.committee_members.first.committee_role.is_program_head).to eq true
+      expect(admin_author.submissions.first.committee_members.first.name).to match /Fake Person/
+      expect(admin_author.submissions.first.committee_members.first.email).to match /abc.*@psu.edu/
+      expect(admin_author.submissions.first.committee_members.first.access_id).to match /abc.*/
+      expect(admin_author.submissions.first.committee_members.first.lionpath_updated_at).to be_truthy
+    end
   end
 
-  it 'generates a dissertation' do
-
+  context 'when degree_type param is a dissertation' do
+    it 'generates a dissertation' do
+      FactoryBot.create :degree, degree_type: DegreeType.default
+      FactoryBot.create :program
+      FactoryBot.create :committee_role, degree_type: DegreeType.default, code: 'XYZ'
+      expect{ described_class.new('adminflow', DegreeType.default).create_submission }.to change(Submission, :count).by 1
+      expect(admin_author.submissions.first.degree_type.slug).to eq 'dissertation'
+      expect(admin_author.submissions.first.lionpath_updated_at).to be_truthy
+      expect(admin_author.submissions.first.committee_members.count).to eq 6
+      expect(admin_author.submissions.first.committee_members.first.name).to match /Fake Person/
+      expect(admin_author.submissions.first.committee_members.first.email).to match /abc.*@psu.edu/
+      expect(admin_author.submissions.first.committee_members.first.access_id).to match /abc.*/
+      expect(admin_author.submissions.first.committee_members.first.lionpath_updated_at).to be_truthy
+      expect(admin_author.submissions.first.committee_members.first.committee_role.is_program_head).to eq true
+      expect(admin_author.submissions.first.lionpath_updated_at).to be_truthy
+      expect(admin_author.submissions.first.committee_members.second.committee_role.is_program_head).to eq false
+      expect(admin_author.submissions.first.committee_members.second.committee_role.code).to eq 'XYZ'
+      expect(admin_author.submissions.first.committee_members.second.lionpath_updated_at).to be_truthy
+    end
   end
 end
