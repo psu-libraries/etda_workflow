cd659082cbb9e2cde554d05374a6709ed1c8e4d6
Alex Kiessling <ajkiessl@gmail.com>
2021-05-04 12:25:55 -0400

    ETDA-1734 Style chagnes to get vertical radio buttons to align vertically

--- app/javascript/packs/base.js
+++ app/javascript/packs/base.js
@@ -32,3 +32,4 @@ require('../base/initialize_tooltips.js');
 require('../base/aria_defended_date.js');
 require('../base/fees_confirm_dialog.js');
 require('../base/collapse_indicators.js');
+require('../base/toggle_caret.js');
