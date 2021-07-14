4bde1ebfe2f8dcf366fd8145764341af6164ea0a
Alex Kiessling <ajkiessl@gmail.com>
2021-07-14 16:58:40 -0400

    rubocop fix

--- spec/services/solr_data_import_service_spec.rb
+++ spec/services/solr_data_import_service_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe SolrDataImportService, type: :model do
   it 'executes command and returns an error' do
     solr = solr_data_import_service.send('solr')
     error_result = { :error => true, "statusMessages" => { "" => "An error occurred! Check the log messages for more information" } }
-    allow(solr).to receive(:get).with("#{current_partner.id}_core/dataimport", params: {"clean"=>false, "command"=>"delta-import"}).and_return(error_result)
+    allow(solr).to receive(:get).with("#{current_partner.id}_core/dataimport", params: {"clean" => false, "command" => 'delta-import'}).and_return(error_result)
     expect(solr_data_import_service.send('execute_cmd', 'command' => 'delta-import', 'clean' => false)).to eql(error_result)
   end
 
