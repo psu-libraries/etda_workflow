ed1f524e48a5ed2ad35442715884abdbf5061ff9
Alex Kiessling <ajkiessl@gmail.com>
2021-07-19 17:04:20 -0400

    Working through datatables.  For some reason the data is loading but not rendering.

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,6 +1,10 @@
 class Admin::ReportsController < AdminController
   def custom_report_index
-    @submissions = Submission.all
+    @semester_list = Submission.order('year DESC').pluck(:year, :semester).uniq.map{|str| ["#{str[0].to_s} #{str[1]}"]}
+    if params[:format] == 'json'
+      semester = params[:semester].split(' ')
+      @submissions = Submission.where('submissions.year = ? AND submissions.semester = ?', semester.first, semester.second)
+    end
     respond_to do |format|
       format.html
       format.json
@@ -13,25 +17,6 @@ class Admin::ReportsController < AdminController
     respond_to do |format|
       format.csv { render template: 'admin/reports/csv_export_report.csv.erb' }
       headers['Content-Disposition'] = 'attachment; filename="custom_report.csv"'
-      headers['Content-Type'] ||= 'text/csv'
-      headers['Content-Type'] ||= 'text/xls'
-    end
-  end
-
-  def committee_report_index
-    @submissions = Submission.released_for_publication
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
-
-  def committee_report_export
-    ids = params[:submission_ids].split(',').map(&:to_i)
-    @csv_report_export = ExportCsv.new('committee_report', Submission.where(id: ids))
-    respond_to do |format|
-      format.csv { render template: 'admin/reports/csv_export_report.csv.erb' }
-      headers['Content-Disposition'] = 'attachment; filename="committee_report.csv"'
       headers['Content-Type'] ||= 'text/csv'
       headers['Content-Type'] ||= 'text/xls'
     end
