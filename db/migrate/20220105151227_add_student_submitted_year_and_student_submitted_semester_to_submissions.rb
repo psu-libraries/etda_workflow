6f8a9659a98a558511a3e3fc932be73dc07b7edc
Alex Kiessling <ajkiessl@gmail.com>
2022-01-05 15:35:34 -0500

    Added logic to migration to update author_submitted semester and year with the data in semester and year.  Admin submission edit page changed to show both lionpath and author semester and year.  Needs tests.

--- db/migrate/20220105151227_add_student_submitted_year_and_student_submitted_semester_to_submissions.rb
+++ db/migrate/20220105151227_add_student_submitted_year_and_student_submitted_semester_to_submissions.rb
@@ -2,6 +2,10 @@ class AddStudentSubmittedYearAndStudentSubmittedSemesterToSubmissions < ActiveRe
   def self.up
     add_column :submissions, :author_submitted_year, :integer
     add_column :submissions, :author_submitted_semester, :string
+    Submission.find_each do |submission|
+      submission.update author_submitted_year: submission.year
+      submission.update author_submitted_semester: submission.semester
+    end
   end
 
   def self.down
