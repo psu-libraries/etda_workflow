0e28b35332828aa441ab4b48609b356d12976eb7
Alex Kiessling <ajkiessl@gmail.com>
2022-03-14 16:35:22 -0400

    Hotfix:  Using degree.name when sending code to gradsch webservice instead of degree_type.name.  We need to pick up things like DED, MA, etc.

--- spec/services/fee_payment_service_spec.rb
+++ spec/services/fee_payment_service_spec.rb
@@ -4,10 +4,15 @@ require 'shoulda-matchers'
 RSpec.describe FeePaymentService do
   let(:service) { described_class.new(submission) }
   let!(:submission) { create :submission }
+  before do
+    submission.degree.name = 'PHD'
+    submission.save!
+  end
 
   context "when the student's fee has been paid" do
-    context "when the submission degree_type is Dissertation" do
+    context "when the submission degree is PHD" do
       it 'returns true' do
+
         stub_request(:get, "https://secure.gradsch.psu.edu/services/etd/etdPayment.cfm?degree=PHD&psuid=#{submission.author.psu_idn}")
           .with(
             headers: {
@@ -21,9 +26,9 @@ RSpec.describe FeePaymentService do
       end
     end
 
-    context "when the submission degree_type is Master Thesis" do
+    context "when the submission degree is MS" do
       it 'returns true' do
-        submission.degree.degree_type = DegreeType.find_by(name: 'Master Thesis')
+        submission.degree.name = 'MS'
         submission.save!
         stub_request(:get, "https://secure.gradsch.psu.edu/services/etd/etdPayment.cfm?degree=MS&psuid=#{submission.author.psu_idn}")
           .with(
@@ -34,6 +39,23 @@ RSpec.describe FeePaymentService do
             }
           )
           .to_return(status: 200, body: "\r\n    {\"data\":[{\"ETDPAYMENTFOUND\":\"Y\"}],\"error\":\"\"}\r\n    ", headers: {})
+        expect(service.fee_is_paid?).to eq true
+      end
+    end
+
+    context "when the submission degree is DED" do
+      it 'returns true' do
+        submission.degree.name = 'DED'
+        submission.save!
+        stub_request(:get, "https://secure.gradsch.psu.edu/services/etd/etdPayment.cfm?degree=DED&psuid=#{submission.author.psu_idn}")
+            .with(
+                headers: {
+                    'Accept' => '*/*',
+                    'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                    'User-Agent' => 'Ruby'
+                }
+            )
+            .to_return(status: 200, body: "\r\n    {\"data\":[{\"ETDPAYMENTFOUND\":\"Y\"}],\"error\":\"\"}\r\n    ", headers: {})
         expect(service.fee_is_paid?).to eq true
       end
     end
