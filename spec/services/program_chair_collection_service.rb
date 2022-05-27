e00eb096ae7f09706a162987d5c58347de515535
Alex Kiessling <ajkiessl@gmail.com>
2022-05-27 16:22:04 -0400

    Adds ProgramChairCollectionService spec

---
+++ spec/services/program_chair_collection_service.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'shoulda-matchers'
+
+RSpec.describe ProgramChairCollectionService do
+  let(:pcc_service) { described_class.new(submission) }
+
+
+end
