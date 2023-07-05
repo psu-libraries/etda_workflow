7c022581277d3ebcfc0d3084405e75ff6c6743d1
Alex Kiessling <ajkiessl@gmail.com>
2023-07-05 12:43:39 -0400

    Revamped this broken test to fix it

--- spec/integration/author/status_giver/collecting_committee_spec.rb
+++ spec/integration/author/status_giver/collecting_committee_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'When Collecting Committee status', type: :integration, js: true 
   describe "author can delete a submission" do
     let!(:degree) { FactoryBot.create :degree, degree_type: DegreeType.default }
     let!(:approval_configuration) { FactoryBot.create :approval_configuration, degree_type: degree.degree_type, head_of_program_is_approving: true }
+    let!(:submission) { FactoryBot.create(:submission, :collecting_format_review_files, author:) }
     let(:author) { current_author }
 
     before do
@@ -11,13 +12,13 @@ RSpec.describe 'When Collecting Committee status', type: :integration, js: true 
     end
 
     it "deletes the submission", honors: true, sset: true, milsch: true do
-      FactoryBot.create(:submission, :collecting_format_review_files, author:)
       start_count = author.submissions.count
       expect(start_count > 0).to be_truthy
       visit author_root_path
       if !current_partner.graduate?
         click_link("delete")
-        sleep 1
+        page.driver.browser.switch_to.alert.accept
+        sleep 0.5
         expect(author.reload.submissions.count).to eq(start_count - 1)
       else
         expect(page).not_to have_link('delete')
