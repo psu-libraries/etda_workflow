86ae7de8e778580361df802982ea3e2a23f8c0f7
whereismyjetpack <djb44@psu.edu>
2023-07-18 12:55:07 -0400

    attempt to add auth to DirectoryService calls

--- app/models/directory_service.rb
+++ app/models/directory_service.rb
@@ -8,10 +8,8 @@ class DirectoryService
       nil
     end
 
-    private
-
       def endpoint_url
-        "https://prod.apps.psu.edu/directory-service-web/resources/people"
+        "#{endpoint}/directory-service-web/resources/people"
       end
 
       def query(email_address)
@@ -23,7 +21,34 @@ class DirectoryService
       end
 
       def headers
-        { accept: 'application/vnd-psu.edu-v1+json' }
+        { accept: 'application/vnd-psu.edu-v1+json', Authorization: "Bearer #{token}" }
       end
+
+      def token
+        oauth_client = OAuth2::Client.new(client_id,
+                                          client_secret, site: oauth_endpoint,
+                                                         authorize_url: '/oauth/api/authz',
+                                                         token_url: '/oauth/api/token')
+        oauth_token = oauth_client.client_credentials.get_token
+        oauth_token.token
+      end
+
+      def client_id
+        @client_id ||= ENV.fetch('PSU_ID_OAUTH_CLIENT_ID')
+      end
+
+      def client_secret
+        @client_secret ||= ENV.fetch('PSU_ID_OAUTH_CLIENT_SECRET')
+      end
+
+      def oauth_endpoint
+        @oauth_endpoint ||= ENV.fetch('PSU_ID_OAUTH_ENDPOINT', 'https://acceptance-oauth2-server.qa.k8s.psu.edu')
+      end
+
+      def endpoint
+        @endpoint ||= ENV.fetch('DIRECTORY_SERVICE_ENDPOINT', 'https://acceptance-directory-service.qa.k8s.psu.edu')
+      end
+
   end
 end
+
