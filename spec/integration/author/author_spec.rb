bf083fc7ea8af806e9eeb33d688f08ecb0cd44c9
Alex Kiessling <ajkiessl@gmail.com>
2024-05-21 08:54:04 -0400

    Forgot to remove this

--- spec/integration/author/author_spec.rb
+++ spec/integration/author/author_spec.rb
@@ -47,10 +47,6 @@ RSpec.describe 'Author submission page', type: :integration, honors: true, js: t
       expect(page).to have_content('Existing submission found. The status of your previously submitted document is listed below:')
       expect(page).to have_link('Provide Committee')
       expect(page).to have_content('If you would like to start a new thesis') unless current_partner.graduate? || current_partner.honors?
-      if current_partner.honors?
-        new_link = find_link('Start a new Submission')
-        expect(new_link['outerHTML']).to match(/You already have a/)
-      end
     end
   end
 
