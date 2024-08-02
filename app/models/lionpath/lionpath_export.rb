7be8a5fadc7464fbb355a37e743372460c2b81ac
Alex Kiessling <ajkiessl@gmail.com>
2024-08-02 11:46:30 -0400

    Adds lionpath export class and class for generating the payload json for the export

---
+++ app/models/lionpath/lionpath_export.rb
@@ -0,0 +1,36 @@
+require 'httparty'
+
+class LionpathExport
+  def initialize(submission)
+    @payload = LpExportPayload.new(submission).to_json
+  end
+
+  def call
+    options = {
+      body: payload.to_json,
+      headers: {
+        'Content-Type' => 'application/json',
+        'Content-Transfer-Encoding' => 'application/json'
+      },
+      basic_auth: auth
+    }
+    
+    self.class.put(base_uri + endpoint_path, options)
+  end
+
+  private
+    attr_accessor :payload
+
+    def auth
+      { username: ENV['LP_SA_USERNAME'], 
+        password: ENV['LP_SA_PASSWORD'] }
+    end
+
+    def base_uri
+      ENV['LP_SA_HOST']
+    end
+
+    def endpoint_path
+      "/PSIGW/RESTListeningConnector/PSFT_HR/PE_SR199_ETD_INPUT.v1/update"
+    end
+end
---
+++ app/models/lionpath/lionpath_export_payload.rb
@@ -0,0 +1,76 @@
+class LionpathExportPayload
+  def initialize(submission)
+    @submission = submission
+  end
+
+  def to_json
+    {
+      "PE_SR199_ETD_REQ": {
+        "emplid": author.psu_idn,
+        "candNbr": author.candidate_number,
+        "thesisTitle": submission.title,
+        "thesisStatus": thesis_status,
+        "embargoType": embargo_type,
+        "embargoStartDt": submission.released_metadata_at,
+        "embargoEndDt": submission.released_fo_publication_at,
+        "candAdvFlg": committee_and_program_head_approved,
+        "exPymtFlg": payment_received,
+        "libDepFlg": federal_funding_used,
+        "grdtnFlg": committee_and_program_head_approved
+      }
+    }.to_json
+  end
+
+  private
+
+    attr_accessor :submission
+
+    def author
+      submission.author
+    end
+
+    def thesis_status
+      status_behavior = submission.status_behavior
+      return 'SUBMITTED' if status_behavior.beyond_collecting_format_review_files? && 
+                            !status_behavior.beyond_waiting_for_committee_review_rejected?
+
+      return 'APPROVED' if status_behavior.beyond_waiting_for_committee_review_rejected?
+
+      ''
+    end
+
+    def embargo_type
+      case submission.access_level
+      when 'open_access'
+        'OPEN'
+      when 'restricted_to_institution'
+        'RPSU'
+      when 'restricted'
+        'RSTR'
+      else
+      ""
+      end
+    end
+
+    def committee_and_program_head_approved
+      return "Y" if submission.beyond_waiting_for_committee_review_rejected?
+
+      ""
+    end
+
+    def payment_received
+      return "Y" if submission.final_submission_files_uploaded_at.present?
+
+      ""
+    end
+
+    def federal_funding_used
+      return "" if !status_behavior.beyond_waiting_for_committee_review_rejected?
+
+      return "Y" if submission.federal_funding == true
+
+      return "N" if submission.federal_funding == false
+
+      ""
+    end
+end
