ee603111db2d9e417f1be7312348220067fcd7a2
Jesse Landis-Eigsti <jml8735@psu.edu>
2024-12-04 10:10:49 -0700

    857 - wraps the String extensions in a CoreExt module

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,20 +1,22 @@
-class String
-  def strip_control_and_extended_characters
-    chars.each_with_object("") do |char, str|
-      str << char if char.ascii_only? && char.ord.between?(32, 126)
+module CoreExt
+  class String
+    def strip_control_and_extended_characters
+      chars.each_with_object("") do |char, str|
+        str << char if char.ascii_only? && char.ord.between?(32, 126)
+      end
     end
-  end
 
-  def articleize
-    %w[a e i o u].include?(self[0].downcase) ? "an #{self}" : "a #{self}"
-  end
-
-  def numeric?
-    begin
-      Float(self).nil?
-    rescue StandardError
-      return false
+    def articleize
+      %w[a e i o u].include?(self[0].downcase) ? "an #{self}" : "a #{self}"
     end
-    true
+
+    def numeric?
+      begin
+        Float(self).nil?
+      rescue StandardError
+        return false
+      end
+      true
+    end
   end
 end
