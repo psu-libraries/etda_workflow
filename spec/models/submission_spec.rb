a9aa7b4703ea129fea3f40df5d915d9afdd7d3d1
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-01-06 15:34:53 -0700

    950 - removes unneeded test

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -95,7 +95,6 @@ RSpec.describe Submission, type: :model do
   it { is_expected.to accept_nested_attributes_for :committee_members }
   it { is_expected.to accept_nested_attributes_for :format_review_files }
   it { is_expected.to accept_nested_attributes_for :final_submission_files }
-  it { is_expected.to accept_nested_attributes_for :remediated_final_submission_files }
 
   it { is_expected.to delegate_method(:program_name).to(:program).as(:name) }
   it { is_expected.to delegate_method(:degree_name).to(:degree).as(:name) }
