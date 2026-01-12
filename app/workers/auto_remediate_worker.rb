babf7cf289b5648a038e6c09d7a8177f9a3a3f57
Alex Kiessling <ajkiessl@gmail.com>
2026-01-12 16:08:44 -0500

    formulate download url

--- app/workers/auto_remediate_worker.rb
+++ app/workers/auto_remediate_worker.rb
@@ -6,8 +6,12 @@ class AutoRemediateWorker
 
   def perform(final_submission_file_id)
     file = FinalSubmissionFile.find(final_submission_file_id)
+    # There is no public download URL within ETDA Workflow, so we construct one
+    # that points to the ETDA Explore application.  This is somewhat brittle since
+    # it needs to know the URL structure of ETDA Explore, without any shared code.
+    download_url = "#{EtdaUtilities::EtdaFilePaths.explore_url}/files/final_submissions/#{file.id}"
 
-    remediation_job_uuid = PdfRemediation::Client.new(file.file_url).request_remediation
+    remediation_job_uuid = PdfRemediation::Client.new(download_url).request_remediation
     file.update(remediation_job_uuid: remediation_job_uuid)
   end
 end
