110c82521cbbd3ccd219e6a0ed6bc614355233aa
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-01-15 13:13:32 -0700

    967 - creates ExternalApp model and migration

---
+++ db/migrate/20260115201151_create_external_apps.rb
@@ -0,0 +1,10 @@
+class CreateExternalApps < ActiveRecord::Migration[7.2]
+  def change
+    create_table :external_apps do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :external_apps, :name
+  end
+end
