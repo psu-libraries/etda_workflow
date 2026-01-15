3d3f607fe2f160f8440bbee523e492f271d09616
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-01-15 13:38:45 -0700

    967 - adds validation, aliases, and convenience methods

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -1,3 +1,18 @@
 class ApiToken < ApplicationRecord
-  belongs_to :external_app
+  belongs_to :application,
+             class_name: 'ExternalApp',
+             foreign_key: 'application_id',
+             inverse_of: 'api_tokens'
+
+  before_create :set_token
+
+  def record_usage
+    update_column(:last_used_at, Time.zone.now)
+  end
+
+  private
+
+    def set_token
+      self.token ||= SecureRandom.hex(48)
+    end
 end
--- app/models/external_app.rb
+++ app/models/external_app.rb
@@ -1,2 +1,10 @@
 class ExternalApp < ApplicationRecord
+  has_many :api_tokens,
+          dependent: :destroy,
+          foreign_key: 'application_id',
+          inverse_of: 'application'
+
+  validates :name,
+    presence: true,
+    uniqueness: true
 end
