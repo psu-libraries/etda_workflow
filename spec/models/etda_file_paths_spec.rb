efbf4651b6604ba48f53cd992e6acc7142c7b185
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2026-01-23 12:37:48 -0500

    Remediated file paths and movement (#977)
    * I think this should do what I want, but haven't tested it yet
    
    * Fixed uploader case matcher, adjusted main file path for remediated files
    
    * Adds some tests.  Fixes some things up
    
    * Adds some more tests
    
    * Revert file structure generators to old setup.  Use new EtdaUtilties functionality
    
    * Change file path generator for initial uploader to use remediated directory when needed
    
    * Fixed tests
    
    * use dependency injection for move_a_file.  Adds tests for that method
    
    * Adds move_file test
    
    * Clean up code
    
    * Update etda_utilities
    
    * refactor
    
    * Moving files on publish and unpublish fixed to handle remediated files
    
    * Changed full_file_path tests to test current_location instead.  This covers all the public methods for RemediatedFinalSubmissionFile
    
    * Adds tests for public methods in submission_release_service
    
    * Made these tests a little stronger and cleaned them up
    
    * Make file_class a required parameter for move_a_file (no default).  Adds function to delete RemediatedFinalSubmissionFile when object is destroyed
    
    * Adds restricted_liberal_arts

--- spec/models/etda_file_paths_spec.rb
+++ spec/models/etda_file_paths_spec.rb
@@ -41,4 +41,61 @@ RSpec.describe EtdaFilePaths, type: :model do
       expect(described_class.new.explore_open).to eql("#{explore_path}open_access/")
     end
   end
+
+  describe '#move_a_file' do
+    let(:file_paths) { described_class.new }
+    let(:fid) { 123 }
+    let(:original_file_location) { '/tmp/original/file.pdf' }
+    let(:updated_file) do
+      instance_double('FinalSubmissionFile',
+                      full_file_path: '/dest/path/',
+                      asset_identifier: 'file.pdf')
+    end
+
+    context 'when the original file does not exist' do
+      it 'returns an error message and does not look up the file' do
+        allow(File).to receive(:exist?).with(original_file_location).and_return(false)
+        allow(Rails.logger).to receive(:error)
+        file_class = class_double('FinalSubmissionFile')
+        allow(file_class).to receive(:find)
+
+        result = file_paths.move_a_file(fid, original_file_location, file_class: file_class)
+
+        expect(result).to eq("File not found: #{original_file_location}")
+        expect(file_class).not_to have_received(:find)
+      end
+    end
+
+    context 'when the original file exists' do
+      it 'moves the file using the provided file_class' do
+        file_class = class_double('RemediatedFinalSubmissionFile').as_stubbed_const
+
+        allow(File).to receive(:exist?).with(original_file_location).and_return(true)
+        allow(file_class).to receive(:find).with(fid).and_return(updated_file)
+        allow(FileUtils).to receive(:mkpath)
+        allow(FileUtils).to receive(:mv)
+
+        result = file_paths.move_a_file(fid, original_file_location, file_class: file_class)
+
+        expect(FileUtils).to have_received(:mkpath).with('/dest/path/')
+        expect(FileUtils).to have_received(:mv).with(original_file_location, '/dest/path/file.pdf')
+        expect(result).to eq('')
+      end
+    end
+
+    context 'when using FinalSubmissionFile as the file_class' do
+      it 'uses FinalSubmissionFile to find the record' do
+        file_class = class_double('FinalSubmissionFile').as_stubbed_const
+
+        allow(File).to receive(:exist?).with(original_file_location).and_return(true)
+        allow(file_class).to receive(:find).with(fid).and_return(updated_file)
+        allow(FileUtils).to receive(:mkpath)
+        allow(FileUtils).to receive(:mv)
+
+        file_paths.move_a_file(fid, original_file_location, file_class: file_class)
+
+        expect(file_class).to have_received(:find).with(fid)
+      end
+    end
+  end
 end
