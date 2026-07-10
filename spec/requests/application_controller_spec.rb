c5a9bffd5489097db012204d6ff20d8a969777f6
Jesse Landis-Eigsti <jml8735@psu.edu>
2026-07-10 10:55:36 -0600

    1105 - modified failing test
    The ActionDispatch::Request::Session can no longer be mocked
    by an empty hash, so I replaced it with an instance double.
    I put it in an rspec shared_context in case we need it again.

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 require 'devise/test/integration_helpers'
+require 'support/session_double'
 
 RSpec.describe 'Devise Login', type: :request do
   include Devise::Test::IntegrationHelpers
@@ -12,42 +13,45 @@ RSpec.describe 'Devise Login', type: :request do
     allow(request).to receive_messages(controller: "admin/degrees", headers: { 'HTTP_REMOTE_USER' => 'saw140' })
   end
 
-  it 'signs author in and out' do
-    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({})
-    headers = { 'HTTP_REMOTE_USER' => 'ajk5603', 'REQUEST_URI' => '/author/submissions' }
-    expect(Author.find_by(access_id: 'ajk5603')).to be_nil
-    request.headers.merge! headers
-    Devise::Strategies::OidcAuthenticatable.new(headers).authenticate!
-    get root_path
-    expect(Author.find_by(access_id: 'ajk5603')).not_to be_nil
-  end
+  context 'test environment' do
+    include_context 'session double'
+    it 'signs author in and out' do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session_double)
+      headers = { 'HTTP_REMOTE_USER' => 'ajk5603', 'REQUEST_URI' => '/author/submissions' }
+      expect(Author.find_by(access_id: 'ajk5603')).to be_nil
+      request.headers.merge! headers
+      Devise::Strategies::OidcAuthenticatable.new(headers).authenticate!
+      get root_path
+      expect(Author.find_by(access_id: 'ajk5603')).not_to be_nil
+    end
 
-  it 'signs admin in and out' do
-    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({})
-    headers = { 'HTTP_REMOTE_USER' => 'xxb13', 'REQUEST_URI' => '/admin/degrees' }
-    expect(Admin.find_by(access_id: 'xxb13')).to be_nil
-    request.headers.merge! headers
-    Devise::Strategies::OidcAuthenticatable.new(headers).authenticate!
-    get root_path
-    expect(Admin.find_by(access_id: 'xxb13')).not_to be_nil
-  end
+    it 'signs admin in and out' do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session_double)
+      headers = { 'HTTP_REMOTE_USER' => 'xxb13', 'REQUEST_URI' => '/admin/degrees' }
+      expect(Admin.find_by(access_id: 'xxb13')).to be_nil
+      request.headers.merge! headers
+      Devise::Strategies::OidcAuthenticatable.new(headers).authenticate!
+      get root_path
+      expect(Admin.find_by(access_id: 'xxb13')).not_to be_nil
+    end
 
-  it 'signs approver in and out' do
-    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return({})
-    headers = { 'HTTP_REMOTE_USER' => 'ajk5603', 'REQUEST_URI' => '/approver' }
-    expect(Approver.find_by(access_id: 'ajk5603')).to be_nil
-    request.headers.merge! headers
-    Devise::Strategies::OidcAuthenticatable.new(headers).authenticate!
-    get root_path
-    expect(Approver.find_by(access_id: 'ajk5603')).not_to be_nil
-  end
+    it 'signs approver in and out' do
+      allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session_double)
+      headers = { 'HTTP_REMOTE_USER' => 'ajk5603', 'REQUEST_URI' => '/approver' }
+      expect(Approver.find_by(access_id: 'ajk5603')).to be_nil
+      request.headers.merge! headers
+      Devise::Strategies::OidcAuthenticatable.new(headers).authenticate!
+      get root_path
+      expect(Approver.find_by(access_id: 'ajk5603')).not_to be_nil
+    end
 
-  it 'does not authenticate an admin who is not in ldap admin group' do
-    headers = { 'HTTP_REMOTE_USER' => 'saw140', 'REQUEST_URI' => '/admin/degrees' }
-    expect(Admin.find_by(access_id: 'saw140')).to be_nil
-    request.headers.merge! headers
-    Devise::Strategies::OidcAuthenticatable.new(headers).authenticate! if LdapUniversityDirectory.new.in_admin_group? 'saw140'
-    expect(Admin.find_by(access_id: 'saw140')).to be_nil
+    it 'does not authenticate an admin who is not in ldap admin group' do
+      headers = { 'HTTP_REMOTE_USER' => 'saw140', 'REQUEST_URI' => '/admin/degrees' }
+      expect(Admin.find_by(access_id: 'saw140')).to be_nil
+      request.headers.merge! headers
+      Devise::Strategies::OidcAuthenticatable.new(headers).authenticate! if LdapUniversityDirectory.new.in_admin_group? 'saw140'
+      expect(Admin.find_by(access_id: 'saw140')).to be_nil
+    end
   end
 
   context 'production environment' do
